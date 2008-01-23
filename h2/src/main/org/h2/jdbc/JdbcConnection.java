add5ed0a89df2c522ab4a7dd7541e77dbc4ccfe6
Thomas Mueller <thomasmueller@users.noreply.github.com>
2008-01-23 19:41:44 +0000

    copyright 2008

--- h2/src/main/org/h2/jdbc/JdbcConnection.java
+++ h2/src/main/org/h2/jdbc/JdbcConnection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2007 H2 Group. Licensed under the H2 License, Version 1.0 (http://h2database.com/html/license.html).
+ * Copyright 2004-2008 H2 Group. Licensed under the H2 License, Version 1.0 (http://h2database.com/html/license.html).
  * Initial Developer: H2 Group
  */
 package org.h2.jdbc;
@@ -903,16 +903,24 @@ public class JdbcConnection extends TraceObject implements Connection {
      * INTERNAL
      */
     public JdbcConnection(String url, Properties info) throws SQLException {
+        this(new ConnectionInfo(url, info), true);
+    }
+
+    /**
+     * INTERNAL
+     */
+    public JdbcConnection(ConnectionInfo ci, boolean useBaseDir) throws SQLException {
         try {
             checkJavaVersion();
-            ConnectionInfo ci = new ConnectionInfo(url, info);
             if (ci.isRemote()) {
                 session = new SessionRemote().createSession(ci);
             } else {
                 SessionInterface si = (SessionInterface) ClassUtils.loadSystemClass("org.h2.engine.Session").newInstance();
-                String baseDir = SysProperties.getBaseDir();
-                if (baseDir != null) {
-                    ci.setBaseDir(baseDir);
+                if (useBaseDir) {
+                    String baseDir = SysProperties.getBaseDir();
+                    if (baseDir != null) {
+                        ci.setBaseDir(baseDir);
+                    }
                 }
                 session = si.createSession(ci);
             }
@@ -922,7 +930,7 @@ public class JdbcConnection extends TraceObject implements Connection {
             this.user = ci.getUserName();
             if (info()) {
                 trace.infoCode("Connection " + getTraceObjectName()
-                        + " = DriverManager.getConnection(" + quote(url)
+                        + " = DriverManager.getConnection(" + quote(ci.getOriginalURL())
                         + ", " + quote(user) + ", \"\")");
             }
             this.url = ci.getURL();

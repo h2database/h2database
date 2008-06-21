2ae4458597fd44d9977fb108bc35af9eb8c0f4e9
Thomas Mueller <thomasmueller@users.noreply.github.com>
2008-06-21 15:32:28 +0000

    prepare for next release

--- h2/src/main/org/h2/engine/Constants.java
+++ h2/src/main/org/h2/engine/Constants.java
@@ -73,22 +73,22 @@ public class Constants {
     /**
      * The build id is incremented for each public release.
      */
-    public static final int BUILD_ID = 73;
+    public static final int BUILD_ID = 74;
 
     /**
      * The build id of the previous release.
      */
-    public static final int BUILD_ID_PREVIOUS = 72;
+    public static final int BUILD_ID_PREVIOUS = 73;
 
     /**
      * The build date is updated for each public release.
      */
-    public static final String BUILD_DATE = "2008-05-31";
+    public static final String BUILD_DATE = "2008-06-21";
 
     /**
      * The build date is updated for each public release.
      */
-    public static final String BUILD_DATE_PREVIOUS = "2008-05-10";
+    public static final String BUILD_DATE_PREVIOUS = "2008-05-31";
 
     /**
      * The TCP protocol version number 5. This protocol is used by the TCP

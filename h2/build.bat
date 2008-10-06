b57541fe4e674c26134cc9fe6e7474b820adc64f
Thomas Mueller <thomasmueller@users.noreply.github.com>
2008-10-06 17:39:35 +0000

    The build didn't work if the directory temp didn't exist before.

--- h2/build.bat
+++ h2/build.bat
@@ -1,7 +1,8 @@
 @echo off
 if "%JAVA_HOME%"=="" echo Error: JAVA_HOME is not defined.
-if exist bin/org/h2/build/Build.class goto buildOK
+if not exist temp mkdir temp
 if not exist bin mkdir bin
+if exist bin/org/h2/build/Build.class goto buildOK
 javac -sourcepath src/tools -d bin src/tools/org/h2/build/*.java
 :buildOK
 "%JAVA_HOME%/bin/java" -Xmx512m -cp "bin;%JAVA_HOME%/lib/tools.jar;temp" org.h2.build.Build %*

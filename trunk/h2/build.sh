0cf6d1867742540543588be0741d275e87628922
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2008-10-06 17:39:35 +0000

    The build didn't work if the directory temp didn't exist before.

--- trunk/h2/build.sh
+++ trunk/h2/build.sh
@@ -2,10 +2,13 @@
 if [ -z "$JAVA_HOME" ] ; then
   echo "Error: JAVA_HOME is not defined."
 fi
+if [ ! -d "temp" ] ; then
+  mkdir temp
+fi
+if [ ! -d "bin" ] ; then
+  mkdir bin
+fi
 if [ ! -f "bin/org/h2/build/Build.class" ] ; then
-  if [ ! -d "bin" ] ; then
-    mkdir bin
-  fi
   javac -sourcepath src/tools -d bin src/tools/org/h2/build/*.java
 fi
 "$JAVA_HOME/bin/java" -Xmx512m -cp "bin:$JAVA_HOME/lib/tools.jar:temp" org.h2.build.Build $@

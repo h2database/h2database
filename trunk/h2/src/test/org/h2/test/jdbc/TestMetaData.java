461545683d0267d2f8df7a5d40f77a699854980b
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2008-10-07 19:11:44 +0000

    New system table INFORMATION_SCHEMA.SESSION_STATE.

--- trunk/h2/src/test/org/h2/test/jdbc/TestMetaData.java
+++ trunk/h2/src/test/org/h2/test/jdbc/TestMetaData.java
@@ -28,6 +28,15 @@ public class TestMetaData extends TestBase {
     private DatabaseMetaData meta;
     private Statement stat;
     private String catalog = "METADATA";
+    
+    /**
+     * Run just this test.
+     * 
+     * @param a ignored
+     */
+    public static void main(String[] a) throws Exception {
+        TestBase.createCaller().init().test();
+    }
 
     public void test() throws SQLException {
         deleteDb("metaData");
@@ -118,6 +127,8 @@ public class TestMetaData extends TestBase {
         assertEquals("SEQUENCES", rs.getString("TABLE_NAME"));
         rs.next();
         assertEquals("SESSIONS", rs.getString("TABLE_NAME"));
+        rs.next();
+        assertEquals("SESSION_STATE", rs.getString("TABLE_NAME"));
         rs.next();
         assertEquals("SETTINGS", rs.getString("TABLE_NAME"));
         rs.next();

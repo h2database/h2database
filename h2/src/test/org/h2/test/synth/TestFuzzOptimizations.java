1173ab9b2fde29e9286212674947bef33acc98cc
Thomas Mueller <thomasmueller@users.noreply.github.com>
2008-10-10 12:04:44 +0000

    More tests

--- h2/src/test/org/h2/test/synth/TestFuzzOptimizations.java
+++ h2/src/test/org/h2/test/synth/TestFuzzOptimizations.java
@@ -11,6 +11,7 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Random;
 
+import org.h2.constant.SysProperties;
 import org.h2.test.TestBase;
 import org.h2.test.db.Db;
 import org.h2.test.db.Db.Prepared;
@@ -19,7 +20,7 @@ import org.h2.test.db.Db.Prepared;
  * This test executes random SQL statements to test if optimizations are working
  * correctly.
  */
-public class TestOptimizations extends TestBase {
+public class TestFuzzOptimizations extends TestBase {
 
     private Connection conn;
     
@@ -36,7 +37,38 @@ public class TestOptimizations extends TestBase {
         deleteDb("optimizations");
         conn = getConnection("optimizations");
         testGroupSorted();
+        testInSelect();
         conn.close();
+    }
+    
+    private void testInSelect() throws SQLException {
+        boolean old = SysProperties.optimizeInJoin;
+        Db db = new Db(conn);
+        db.execute("CREATE TABLE TEST(A INT, B INT)");
+        db.execute("CREATE INDEX IDX ON TEST(A)");
+        db.execute("INSERT INTO TEST SELECT X/4, MOD(X, 4) FROM SYSTEM_RANGE(1, 16)");
+        db.execute("UPDATE TEST SET A = NULL WHERE A = 0");
+        db.execute("UPDATE TEST SET B = NULL WHERE B = 0");
+        Random random = new Random();
+        long seed = random.nextLong();
+        println("seed: " + seed);
+        for (int i = 0; i < 100; i++) {
+            String sql = "SELECT * FROM TEST T WHERE ";
+            sql += random.nextBoolean() ? "A" : "B";
+            sql += " IN(SELECT ";
+            sql += new String[] { "NULL", "0", "A", "B" }[random.nextInt(4)];
+            sql += " FROM TEST I WHERE I.";
+            sql += random.nextBoolean() ? "A" : "B";
+            sql += "=?) ORDER BY 1, 2";
+            int v = random.nextInt(3);
+            SysProperties.optimizeInJoin = false;
+            List a = db.prepare(sql).set(v).query();
+            SysProperties.optimizeInJoin = true;
+            List b = db.prepare(sql).set(v).query();
+            assertTrue(a.equals(b));
+        }
+        db.execute("DROP TABLE TEST");
+        SysProperties.optimizeInJoin = old;
     }
 
     private void testGroupSorted() throws SQLException {
@@ -92,6 +124,7 @@ public class TestOptimizations extends TestBase {
             }
             db.execute("DROP TABLE TEST_INDEXED");
         }
+        db.execute("DROP TABLE TEST");
     }
     
 }

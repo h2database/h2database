f0ed07707653e15c4c0d98dc53c5657fb994c6ee
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2008-11-07 13:36:11 +0000

    Version 1.1.103 (2008-11-07)

---
+++ tags/version-1.1.103/h2/src/main/org/h2/engine/RightOwner.java
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2004-2008 H2 Group. Multiple-Licensed under the H2 License, 
+ * Version 1.0, and under the Eclipse Public License, Version 1.0
+ * (http://h2database.com/html/license.html).
+ * Initial Developer: H2 Group
+ */
+package org.h2.engine;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.h2.table.Table;
+
+/**
+ * A right owner (sometimes called principal).
+ */
+public abstract class RightOwner extends DbObjectBase {
+
+    /**
+     * The map of granted roles.
+     * The key is the role,
+     * and the value is the right.
+     */
+    private HashMap grantedRoles;
+
+    /**
+     * The map of granted rights.
+     * The key is the table,
+     * and the value is the right.
+     */
+    private HashMap grantedRights;
+
+    protected RightOwner(Database database, int id, String name, String traceModule) {
+        initDbObjectBase(database, id, name, traceModule);
+    }
+
+    /**
+     * Check if a role has been granted for this right owner.
+     * 
+     * @param grantedRole the role
+     * @return true if the role has been granted
+     */
+    public boolean isRoleGranted(Role grantedRole) {
+        if (grantedRole == this) {
+            return true;
+        }
+        if (grantedRoles != null) {
+            Iterator it = grantedRoles.keySet().iterator();
+            while (it.hasNext()) {
+                Role role = (Role) it.next();
+                if (role == grantedRole) {
+                    return true;
+                }
+                if (role.isRoleGranted(grantedRole)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Check if a right is already granted to this object or to objects that
+     * were granted to this object.
+     * 
+     * @param table the table to check
+     * @param rightMask the right mask to check
+     * @return true if the right was already granted
+     */
+    boolean isRightGrantedRecursive(Table table, int rightMask) {
+        Right right;
+        if (grantedRights != null) {
+            right = (Right) grantedRights.get(table);
+            if (right != null) {
+                if ((right.getRightMask() & rightMask) == rightMask) {
+                    return true;
+                }
+            }
+        }
+        if (grantedRoles != null) {
+            Iterator it = grantedRoles.keySet().iterator();
+            while (it.hasNext()) {
+                RightOwner role = (RightOwner) it.next();
+                if (role.isRightGrantedRecursive(table, rightMask)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Grant a right for the given table. Only one right object per table is
+     * supported.
+     * 
+     * @param table the table
+     * @param right the right
+     */
+    public void grantRight(Table table, Right right) {
+        if (grantedRights == null) {
+            grantedRights = new HashMap();
+        }
+        grantedRights.put(table, right);
+    }
+
+    /**
+     * Revoke the right for the given table.
+     * 
+     * @param table the table
+     */
+    public void revokeRight(Table table) {
+        if (grantedRights == null) {
+            return;
+        }
+        grantedRights.remove(table);
+        if (grantedRights.size() == 0) {
+            grantedRights = null;
+        }
+    }
+
+    /**
+     * Grant a role to this object.
+     * 
+     * @param role the role
+     * @param right the right to grant
+     */
+    public void grantRole(Role role, Right right) {
+        if (grantedRoles == null) {
+            grantedRoles = new HashMap();
+        }
+        grantedRoles.put(role, right);
+    }
+
+    /**
+     * Remove the right for the given role.
+     * 
+     * @param role the role to revoke
+     * @throws SQLException if the right has not been granted
+     */
+    public void revokeRole(Role role) {
+        if (grantedRoles == null) {
+            return;
+        }
+        Right right = (Right) grantedRoles.get(role);
+        if (right == null) {
+            return;
+        }
+        grantedRoles.remove(role);
+        if (grantedRoles.size() == 0) {
+            grantedRoles = null;
+        }
+    }
+
+    /**
+     * Get the 'grant table' right of this object.
+     * 
+     * @param table the granted table
+     * @return the right or null if the right has not been granted
+     */
+    public Right getRightForTable(Table table) {
+        if (grantedRights == null) {
+            return null;
+        }
+        return (Right) grantedRights.get(table);
+    }
+
+    /**
+     * Get the 'grant role' right of this object.
+     * 
+     * @param role the granted role
+     * @return the right or null if the right has not been granted
+     */
+    public Right getRightForRole(Role role) {
+        if (grantedRoles == null) {
+            return null;
+        }
+        return (Right) grantedRoles.get(role);
+    }
+
+}
---
+++ tags/version-1.1.103/h2/src/test/org/h2/test/unit/TestSampleApps.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2004-2008 H2 Group. Multiple-Licensed under the H2 License, 
+ * Version 1.0, and under the Eclipse Public License, Version 1.0
+ * (http://h2database.com/html/license.html).
+ * Initial Developer: H2 Group
+ */
+package org.h2.test.unit;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.h2.test.TestBase;
+import org.h2.tools.DeleteDbFiles;
+import org.h2.util.StringUtils;
+
+/**
+ * Tests the sample apps.
+ */
+public class TestSampleApps extends TestBase {
+
+    public void test() throws Exception {
+        deleteDb("optimizations");
+        String url = "jdbc:h2:" + baseDir + "/optimizations";
+        testApp(org.h2.tools.RunScript.class, new String[] { "-url", url, "-user", "sa", "-password", "sa", "-script",
+                "src/test/org/h2/samples/optimizations.sql", "-checkResults" }, "");
+        
+        testApp(org.h2.samples.Compact.class, null, "Compacting...\nDone.");
+        testApp(org.h2.samples.CsvSample.class, null, "NAME: Bob Meier\n" + "EMAIL: bob.meier@abcde.abc\n"
+                + "PHONE: +41123456789\n\n" + "NAME: John Jones\n" + "EMAIL: john.jones@abcde.abc\n"
+                + "PHONE: +41976543210\n");
+        testApp(org.h2.samples.Function.class, null,
+                "2 is prime\n3 is prime\n5 is prime\n7 is prime\n11 is prime\n13 is prime\n17 is prime\n19 is prime");
+        // Not compatible with PostgreSQL JDBC driver (throws a NullPointerException)
+        //testApp(org.h2.samples.SecurePassword.class, null, "Joe");
+        // TODO test ShowProgress (percent numbers are hardware specific)
+        // TODO test ShutdownServer (server needs to be started in a separate
+        // process)
+        testApp(org.h2.samples.TriggerSample.class, null, "The sum is 20.00");
+
+        // tools
+        testApp(org.h2.tools.ChangeFileEncryption.class, new String[] { "-help" },
+                "Allows changing the database file encryption password or algorithm*");
+        testApp(org.h2.tools.ChangeFileEncryption.class, null, 
+                "Allows changing the database file encryption password or algorithm*");
+        testApp(org.h2.tools.DeleteDbFiles.class, new String[] { "-help" },
+                "Deletes all files belonging to a database.*");
+    }
+
+    private void testApp(Class clazz, String[] args, String expected) throws Exception {
+        DeleteDbFiles.execute("data", "test", true);
+        Method m = clazz.getMethod("main", new Class[] { String[].class });
+        PrintStream oldOut = System.out, oldErr = System.err;
+        ByteArrayOutputStream buff = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(buff, false, "UTF-8");
+        System.setOut(out);
+        System.setErr(out);
+        try {
+            m.invoke(null, new Object[] { args });
+        } catch (InvocationTargetException e) {
+            TestBase.logError("error", e.getTargetException());
+        } catch (Throwable e) {
+            TestBase.logError("error", e);
+        }
+        out.flush();
+        System.setOut(oldOut);
+        System.setErr(oldErr);
+        String s = new String(buff.toByteArray(), "UTF-8");
+        s = StringUtils.replaceAll(s, "\r\n", "\n");
+        s = s.trim();
+        expected = expected.trim();
+        if (expected.endsWith("*")) {
+            expected = expected.substring(0, expected.length() - 1);
+            if (!s.startsWith(expected)) {
+                assertEquals(s.trim(), expected.trim());
+            }
+        } else {
+            assertEquals(s.trim(), expected.trim());
+        }
+    }
+}

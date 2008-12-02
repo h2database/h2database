8ab8732c4a88283a6ded35e1269f0dc969092977
Thomas Mueller <thomasmueller@users.noreply.github.com>
2008-12-02 13:10:26 +0000

    typo

--- h2/src/main/org/h2/jdbcx/JdbcConnectionPool.java
+++ h2/src/main/org/h2/jdbcx/JdbcConnectionPool.java
@@ -15,7 +15,7 @@
  * This program is distributed in the hope that it will be useful, 
  * but WITHOUT ANY WARRANTY; without even the implied 
  * warranty of MERCHANTABILITY or FITNESS FOR A 
- * PARTICULAR PURPOSE. See the GNU General Public 
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public 
  * License for more details.
  */
 package org.h2.jdbcx;

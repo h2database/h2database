fb937d9d6837687464dc488488268a594005d9e5
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2008-12-09 21:04:34 +0000

    new experimental page store

---
+++ trunk/h2/src/main/org/h2/store/PageInputStream.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2004-2008 H2 Group. Multiple-Licensed under the H2 License,
+ * Version 1.0, and under the Eclipse Public License, Version 1.0
+ * (http://h2database.com/html/license.html).
+ * Initial Developer: H2 Group
+ */
+package org.h2.store;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import org.h2.constant.ErrorCode;
+import org.h2.engine.Constants;
+import org.h2.index.Page;
+import org.h2.message.Message;
+
+/**
+ * An output stream that writes into a page store.
+ * The format is:
+ * <ul><li>0-3: parent page id
+ * </li><li>4-4: page type
+ * </li><li>5-8: the next page (if there are more) or length
+ * </li><li>9-remainder: data
+ * </li></ul>
+ * The data format is:
+ * <ul><li>0-0: type (0: end, 1: undo)
+ * </li><li>1-4: page id
+ * </li><li>5-: data
+ * </li></ul>
+ */
+public class PageInputStream extends InputStream {
+
+    private PageStore store;
+    private int parentPage;
+    private int type;
+    private int nextPage;
+    private DataPageBinary page;
+    private boolean endOfFile;
+    private int remaining;
+
+    public PageInputStream(PageStore store, int parentPage, int headPage, int type) {
+        this.store = store;
+        this.parentPage = parentPage;
+        this.type = type;
+        nextPage = headPage;
+        page = store.createDataPage();
+    }
+
+    public int read() throws IOException {
+        byte[] b = new byte[1];
+        int len = read(b);
+        return len < 0 ? -1 : b[0];
+    }
+
+    public int read(byte[] b) throws IOException {
+        return read(b, 0, b.length);
+    }
+
+    public int read(byte[] b, int off, int len) throws IOException {
+        if (len == 0) {
+            return 0;
+        }
+        int read = 0;
+        while (len > 0) {
+            int r = readBlock(b, off, len);
+            if (r < 0) {
+                break;
+            }
+            read += r;
+            off += r;
+            len -= r;
+        }
+        return read == 0 ? -1 : read;
+    }
+
+    private int readBlock(byte[] buff, int off, int len) throws IOException {
+        fillBuffer();
+        if (endOfFile) {
+            return -1;
+        }
+        int l = Math.min(remaining, len);
+        page.read(buff, off, l);
+        remaining -= l;
+        return l;
+    }
+
+    private void fillBuffer() throws IOException {
+        if (remaining > 0 || endOfFile) {
+            return;
+        }
+        if (nextPage == 0) {
+            endOfFile = true;
+            return;
+        }
+        page.reset();
+        try {
+            page = store.readPage(nextPage);
+            int p = page.readInt();
+            int t = page.readByte();
+            boolean last = (t & Page.FLAG_LAST) != 0;
+            t &= ~Page.FLAG_LAST;
+            if (type != t || p != parentPage) {
+                throw Message.getSQLException(
+                        ErrorCode.FILE_CORRUPTED_1,
+                        "type:" + t + " parent:" + p +
+                        " expected type:" + type + " expected parent:" + parentPage);
+            }
+            parentPage = nextPage;
+            if (last) {
+                nextPage = 0;
+                remaining = page.readInt();
+            } else {
+                nextPage = page.readInt();
+            }
+            remaining = Constants.FILE_BLOCK_SIZE - page.length();
+        } catch (SQLException e) {
+            throw Message.convertToIOException(e);
+        }
+    }
+
+}
---
+++ trunk/h2/src/main/org/h2/store/PageOutputStream.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2004-2008 H2 Group. Multiple-Licensed under the H2 License,
+ * Version 1.0, and under the Eclipse Public License, Version 1.0
+ * (http://h2database.com/html/license.html).
+ * Initial Developer: H2 Group
+ */
+package org.h2.store;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.SQLException;
+import org.h2.index.Page;
+import org.h2.message.Message;
+
+/**
+ * An output stream that writes into a page store.
+ */
+public class PageOutputStream extends OutputStream {
+
+    private PageStore store;
+    private int parentPage;
+    private int type;
+    private int pageId;
+    private int nextPage;
+    private DataPageBinary page;
+    private int remaining;
+
+    /**
+     * Create a new page output stream.
+     *
+     * @param store the page store
+     * @param parentPage the parent page id
+     * @param headPage the first page
+     * @param type the page type
+     */
+    public PageOutputStream(PageStore store, int parentPage, int headPage, int type) {
+        this.store = store;
+        this.parentPage = parentPage;
+        this.nextPage = headPage;
+        this.type = type;
+        page = store.createDataPage();
+        initPage();
+    }
+
+    public void write(int b) throws IOException {
+        write(new byte[] { (byte) b });
+    }
+
+    public void write(byte[] b) throws IOException {
+        write(b, 0, b.length);
+    }
+
+    private void initPage() {
+        page.reset();
+        page.writeInt(parentPage);
+        page.writeByte((byte) type);
+        page.writeInt(0);
+        remaining = page.length();
+    }
+
+    public void write(byte[] b, int off, int len) throws IOException {
+        if (len <= 0) {
+            return;
+        }
+        while (len > remaining) {
+            page.write(b, off, remaining);
+            off += remaining;
+            len -= remaining;
+            parentPage = nextPage;
+            nextPage = store.allocatePage();
+            page.setInt(5, nextPage);
+            storePage();
+            initPage();
+        }
+        page.write(b, off, len);
+        remaining -= len;
+    }
+
+    private void storePage() throws IOException {
+        try {
+            store.writePage(pageId, page);
+        } catch (SQLException e) {
+            throw Message.convertToIOException(e);
+        }
+    }
+
+    public void close() throws IOException {
+        page.setPos(4);
+        page.writeByte((byte) (type | Page.FLAG_LAST));
+        page.writeInt(store.getPageSize() - remaining - 9);
+        storePage();
+        store = null;
+    }
+
+//    public void write(byte[] buff, int off, int len) throws IOException {
+//        if (len > 0) {
+//            try {
+//                page.reset();
+//                if (compress != null) {
+//                    if (off != 0 || len != buff.length) {
+//                        byte[] b2 = new byte[len];
+//                        System.arraycopy(buff, off, b2, 0, len);
+//                        buff = b2;
+//                        off = 0;
+//                    }
+//                    int uncompressed = len;
+//                    buff = compress.compress(buff, compressionAlgorithm);
+//                    len = buff.length;
+//                    page.writeInt(len);
+//                    page.writeInt(uncompressed);
+//                    page.write(buff, off, len);
+//                } else {
+//                    page.writeInt(len);
+//                    page.write(buff, off, len);
+//                }
+//                page.fillAligned();
+//                store.write(page.getBytes(), 0, page.length());
+//            } catch (SQLException e) {
+//                throw Message.convertToIOException(e);
+//            }
+//        }
+//    }
+
+}

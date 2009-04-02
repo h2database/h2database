d28db754ba5d6a93cff9c39bff953cfcf7d4a275
Thomas Mueller <thomasmueller@users.noreply.github.com>
2009-04-02 18:38:49 +0000

    H2 Console: the browser system property now supports a list of arguments.

--- h2/src/main/org/h2/util/StartBrowser.java
+++ h2/src/main/org/h2/util/StartBrowser.java
@@ -31,7 +31,13 @@ public class StartBrowser {
         try {
             String browser = SysProperties.BROWSER;
             if (browser != null) {
-                if (osName.indexOf("windows") >= 0) {
+                if (browser.indexOf("%url") >= 0) {
+                    String[] args = StringUtils.arraySplit(browser, ',', false);
+                    for (int i = 0; i < args.length; i++) {
+                        args[i] = StringUtils.replaceAll(args[i], "%url", url);
+                    }
+                    rt.exec(args);
+                } else if (osName.indexOf("windows") >= 0) {
                     rt.exec(new String[] { "cmd.exe", "/C",  browser, url });
                 } else {
                     rt.exec(new String[] { browser, url });
@@ -62,6 +68,7 @@ public class StartBrowser {
             if (osName.indexOf("windows") >= 0) {
                 rt.exec(new String[] { "rundll32", "url.dll,FileProtocolHandler", url });
             } else if (osName.indexOf("mac") >= 0) {
+                // Mac OS: to open a page with Safari, use "open -a Safari"
                 Runtime.getRuntime().exec(new String[] { "open", url });
             } else {
                 String[] browsers = { "firefox", "mozilla-firefox", "mozilla", "konqueror", "netscape", "opera" };

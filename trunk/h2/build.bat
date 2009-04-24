8f9a43bbc2e6fa431e73ddbf8cb703694a35bf7b
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2009-04-24 14:26:47 +0000

    Simplify build script; support 'clean'

--- trunk/h2/build.bat
+++ trunk/h2/build.bat
@@ -1,9 +1,7 @@
 @echo off
 if "%JAVA_HOME%"=="" echo Error: JAVA_HOME is not defined.
-if "%1"=="-Dclean" rmdir /s /q temp | rmdir /s /q bin
+if "%1"=="clean" rmdir /s /q temp | rmdir /s /q bin
 if not exist temp mkdir temp
 if not exist bin mkdir bin
-if exist bin/org/h2/build/Build.class goto buildOK
-javac -sourcepath src/tools -d bin src/tools/org/h2/build/*.java
-:buildOK
+javac -source 1.4 -sourcepath src/tools -d bin src/tools/org/h2/build/*.java
 "%JAVA_HOME%/bin/java" -Xmx256m -cp "bin;%JAVA_HOME%/lib/tools.jar;temp" org.h2.build.Build %*

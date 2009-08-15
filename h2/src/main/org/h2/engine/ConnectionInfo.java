8b13e264aa4831041203a902fcdc6c39bfc9b697
Thomas Mueller <thomasmueller@users.noreply.github.com>
2009-08-15 09:45:41 +0000

    Allow to connect using MVCC=1

--- h2/src/main/org/h2/engine/ConnectionInfo.java
+++ h2/src/main/org/h2/engine/ConnectionInfo.java
@@ -283,7 +283,14 @@ public class ConnectionInfo implements Cloneable {
      */
     public boolean getProperty(String key, boolean defaultValue) {
         String x = getProperty(key, null);
-        return x == null ? defaultValue : Boolean.valueOf(x).booleanValue();
+        if (x == null) {
+            return defaultValue;
+        }
+        // support 0 / 1 (like the parser)
+        if (x.length() == 1 && Character.isDigit(x.charAt(0))) {
+            return Integer.parseInt(x) != 0;
+        }
+        return Boolean.valueOf(x).booleanValue();
     }
 
     /**

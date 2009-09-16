c414e8a08469d99587a95543bb74ad1674cdfdae
Thomas Mueller <thomasmueller@users.noreply.github.com>
2009-09-16 18:32:10 +0000

    Remove superfluous bitwise and (found by FindBugs)

--- h2/src/main/org/h2/security/SHA256.java
+++ h2/src/main/org/h2/security/SHA256.java
@@ -100,7 +100,7 @@ public class SHA256 {
             buff[j] = readInt(bytes, i);
         }
         buff[intLen - 2] = byteLen >>> 29;
-        buff[intLen - 1] = (byteLen << 3) & 0xffffffff;
+        buff[intLen - 1] = byteLen << 3;
         int[] w = new int[64];
         int[] hh = new int[] { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
                 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 };

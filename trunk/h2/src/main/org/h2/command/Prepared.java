898f5ce15027855c56c9da16006a5999552eadf9
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2009-11-14 16:02:25 +0000

    The JDBC client did not detect that it was not talking to an H2 server.

--- trunk/h2/src/main/org/h2/command/Prepared.java
+++ trunk/h2/src/main/org/h2/command/Prepared.java
@@ -102,7 +102,7 @@ public abstract class Prepared {
     public boolean needRecompile() throws SQLException {
         Database db = session.getDatabase();
         if (db == null) {
-            throw Message.getSQLException(ErrorCode.CONNECTION_BROKEN);
+            throw Message.getSQLException(ErrorCode.CONNECTION_BROKEN_1, "database closed");
         }
         // TODO parser: currently, compiling every create/drop/... twice!
         // because needRecompile return true even for the first execution

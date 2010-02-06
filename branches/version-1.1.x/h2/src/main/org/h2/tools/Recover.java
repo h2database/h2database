50181d32c1fc99b4944b42634589481acc8bdc7f
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2010-02-06 11:34:29 +0000

    The Recover tool did not convert correctly convert CLOB data with non-ASCII characters.

--- branches/version-1.1.x/h2/src/main/org/h2/tools/Recover.java
+++ branches/version-1.1.x/h2/src/main/org/h2/tools/Recover.java
@@ -162,7 +162,7 @@ public class Recover extends Tool implements DataHandler {
      * INTERNAL
      */
     public static Reader readClob(String fileName) throws IOException {
-        return new BufferedReader(new InputStreamReader(readBlob(fileName)));
+        return new BufferedReader(new InputStreamReader(readBlob(fileName), "UTF-8"));
     }
 
     /**

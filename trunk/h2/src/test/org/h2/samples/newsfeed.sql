a6387ba40ceade831d1e8f246050e3a07231dbfc
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2010-04-10 08:07:39 +0000

    Prepare release.

--- trunk/h2/src/test/org/h2/samples/newsfeed.sql
+++ trunk/h2/src/test/org/h2/samples/newsfeed.sql
@@ -13,6 +13,19 @@ INSERT INTO CHANNEL VALUES('H2 Database Engine' ,
 
 CREATE TABLE ITEM(ID INT PRIMARY KEY, TITLE VARCHAR, ISSUED TIMESTAMP, DESC VARCHAR);
 
+INSERT INTO ITEM VALUES(83,
+'New version available: 1.2.133 (2010-04-10)', '2010-04-10 12:00:00',
+$$A new version of H2 is available for
+<a href="http://www.h2database.com">download</a>.
+(You may have to click 'Refresh').
+<br />
+For details, see the
+<a href="http://www.h2database.com/html/changelog.html">change log</a>.
+<br />
+For future plans, see the
+<a href="http://www.h2database.com/html/roadmap.html">roadmap</a>.
+$$);
+
 INSERT INTO ITEM VALUES(82,
 'New version available: 1.2.132 (2010-03-21)', '2010-03-21 12:00:00',
 $$A new version of H2 is available for
@@ -154,59 +167,6 @@ For details, see the
 <br />
 For future plans, see the
 <a href="http://www.h2database.com/html/roadmap.html">roadmap</a>.
-$$);
-
-INSERT INTO ITEM VALUES(71,
-'New version available: 1.2.121 (2009-10-11)', '2009-10-11 12:00:00',
-$$A new version of H2 is available for <a href="http://www.h2database.com">download</a>.
-(You may have to click 'Refresh').
-<br />
-<b>Changes and new functionality:</b>
-<ul><li>This is a beta version.
-</li><li>If a database in the old format exists, it is now used.
-</li><li>New system property h2.pageStoreTrim to disable shrinking the database.
-</li><li>Better support GaeVFS (Google App Engine Virtual File System)
-    thanks to Thanks to Vince Bonfanti.
-</li></ul>
-<b>Bugfixes:</b>
-<ul><li>Page store bugs were fixed.
-</li><li>The page store did not work when using Retrotranslator.
-</li><li>CSVREAD didn't close the file. Thanks to Vince Bonfanti for the patch!
-</li></ul>
-For details, see the 'Change Log' at
-http://www.h2database.com/html/changelog.html
-<br />
-For future plans, see the 'Roadmap' page at
-http://www.h2database.com/html/roadmap.html
-$$);
-
-INSERT INTO ITEM VALUES(70,
-'New version available: 1.2.120 (2009-10-04)', '2009-10-04 12:00:00',
-$$A new version of H2 is available for <a href="http://www.h2database.com">download</a>.
-(You may have to click 'Refresh').
-<br />
-<b>Changes and new functionality:</b>
-<ul><li>This is a beta version.
-</li><li>New databases are now stored in a new file format.
-    Existing databases are kept in the old file format.
-</li><li>Databases created by this version can not be opened with older versions.
-</li><li>In version 1.2, the following system properties are enabled by default:
-    h2.pageStore, h2.nullConcatIsNull, h2.optimizeInList.
-</li><li>PostgreSQL compatibility: function LASTVAL() as an alias for IDENTITY().
-</li><li>Linked tables now support default values when inserting, updating or merging.
-</li><li>Possibility to set a vendor id in Constants.java.
-</li><li>Allow writing to linked tables in readonly databases.
-</li></ul>
-<b>Bugfixes:</b>
-<ul><li>Issue 125: Renaming primary keys was not persistent. Fixed.
-</li><li>Issue 124: Hibernate schema validation failed for decimal/numeric columns.
-</li><li>Bugfixes in the page store.
-</li></ul>
-For details, see the 'Change Log' at
-http://www.h2database.com/html/changelog.html
-<br />
-For future plans, see the 'Roadmap' page at
-http://www.h2database.com/html/roadmap.html
 $$);
 
 SELECT 'newsfeed-rss.xml' FILE,

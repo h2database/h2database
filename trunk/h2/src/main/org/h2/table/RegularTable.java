4d92779cd29a2b14b51d69e97155541a34c90083
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2010-05-11 21:11:06 +0000

    New system property h2.selectForUpdateMvcc, the default is false (the feature is disabled).

--- trunk/h2/src/main/org/h2/table/RegularTable.java
+++ trunk/h2/src/main/org/h2/table/RegularTable.java
@@ -385,7 +385,9 @@ public class RegularTable extends TableBase {
         }
         if (!force && database.isMultiVersion()) {
             // MVCC: update, delete, and insert use a shared lock.
-            // Select doesn't lock
+            // Select doesn't lock except when using FOR UPDATE and
+            // the system property h2.selectForUpdateMvcc
+            // is not enabled
             if (exclusive) {
                 exclusive = false;
             } else {

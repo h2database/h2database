f6c0d22918afd6ad91bb544921a2a99694bb2a14
Thomas Mueller <thomasmueller@users.noreply.github.com>
2010-06-18 12:07:41 +0000

    Formatting

--- h2/src/main/org/h2/expression/JavaFunction.java
+++ h2/src/main/org/h2/expression/JavaFunction.java
@@ -84,7 +84,7 @@ public class JavaFunction extends Expression implements FunctionCall {
     public String getSQL() {
         StatementBuilder buff = new StatementBuilder();
         // TODO always append the schema once FUNCTIONS_IN_SCHEMA is enabled
-        if (SysProperties.FUNCTIONS_IN_SCHEMA || 
+        if (SysProperties.FUNCTIONS_IN_SCHEMA ||
                 !functionAlias.getSchema().getName().equals(Constants.SCHEMA_MAIN)) {
             buff.append(Parser.quoteIdentifier(functionAlias.getSchema().getName())).append('.');
         }

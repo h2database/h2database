6259a744344c68ab96ee65b84be1770257e40060
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2010-07-05 20:08:37 +0000

    MULTI_THREADED did not work correctly.

--- trunk/h2/src/test/org/h2/test/db/TestMultiThreadedKernel.java
+++ trunk/h2/src/test/org/h2/test/db/TestMultiThreadedKernel.java
@@ -8,9 +8,13 @@ package org.h2.test.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Random;
 import org.h2.test.TestBase;
 import org.h2.util.JdbcUtils;
+import org.h2.util.New;
 
 /**
  * A multi-threaded test case.
@@ -40,6 +44,8 @@ public class TestMultiThreadedKernel extends TestBase {
         if (config.mvcc) {
             return;
         }
+        deleteDb("multiThreadedKernel");
+        testCache();
         deleteDb("multiThreadedKernel");
         final String url = getURL("multiThreadedKernel;DB_CLOSE_DELAY=-1;MULTI_THREADED=1", true);
         final String user = getUser(), password = getPassword();
@@ -80,6 +86,48 @@ public class TestMultiThreadedKernel extends TestBase {
             throw exception;
         }
         deleteDb("multiThreadedKernel");
+    }
+
+    private void testCache() throws Exception {
+        ArrayList<Thread> list = New.arrayList();
+        int size = 3;
+        final int count = 100;
+        final boolean[] stopped = { false };
+        String url = getURL("multiThreadedKernel;MULTI_THREADED=TRUE;CACHE_SIZE=1", true);
+        for (int i = 0; i < size; i++) {
+            final Connection conn = DriverManager.getConnection(url, getUser(), getPassword());
+            if (i == 0) {
+                Statement stat = conn.createStatement();
+                stat.execute("create table test(id int primary key, name varchar) "
+                        + "as select x, space(3000) from system_range(1, " + count + ")");
+            }
+            final Random random = new Random(i);
+            final int x = i;
+            Thread t = new Thread() {
+                public void run() {
+                    try {
+                        PreparedStatement prep = conn.prepareStatement(
+                                "select * from test where id = ?");
+                        Statement stat = conn.createStatement();
+                        while (!stopped[0]) {
+                            prep.setInt(1, random.nextInt(count));
+                            prep.execute();
+                            stat.execute("create table test" + x + "(id int)");
+                            stat.execute("drop table test" + x);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            t.start();
+            list.add(t);
+        }
+        Thread.sleep(1000);
+        stopped[0] = true;
+        for (Thread t : list) {
+            t.join();
+        }
     }
 
 }

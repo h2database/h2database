40ee48c6da5446e03c8d3568c7a4e267bf79a6bd
Thomas Mueller <thomasmueller@users.noreply.github.com>
2010-10-05 19:46:22 +0000

    The build tool now uses JAVA_HOME for javac as well. Issue 233.

--- h2/build.sh
+++ h2/build.sh
@@ -9,5 +9,5 @@ fi
 if [ "$1" = "clean" ] ; then rm -rf temp bin ; fi
 if [ ! -d "temp" ] ; then mkdir temp ; fi
 if [ ! -d "bin" ] ; then mkdir bin ; fi
-javac -sourcepath src/tools -d bin src/tools/org/h2/build/*.java
+"$JAVA_HOME/bin/javac" -sourcepath src/tools -d bin src/tools/org/h2/build/*.java
 "$JAVA_HOME/bin/java" -Xmx256m -cp "bin:$JAVA_HOME/lib/tools.jar:temp" org.h2.build.Build $@

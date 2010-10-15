36c2d1f2affbe214d5b8424996b8372b50f78119
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2010-10-15 17:20:41 +0000

    Prepare release.

--- trunk/h2/src/main/org/h2/engine/Constants.java
+++ trunk/h2/src/main/org/h2/engine/Constants.java
@@ -14,12 +14,12 @@ public class Constants {
     /**
      * The build id is incremented for each public release.
      */
-    public static final int BUILD_ID = 143;
+    public static final int BUILD_ID = 144;
 
     /**
      * The build id of the last stable release.
      */
-    public static final int BUILD_ID_STABLE = 142;
+    public static final int BUILD_ID_STABLE = 143;
 
     /**
      * If H2 is compiled to be included in a product, this should be set to
@@ -32,12 +32,12 @@ public class Constants {
     /**
      * The build date is updated for each public release.
      */
-    public static final String BUILD_DATE = "2010-09-18";
+    public static final String BUILD_DATE = "2010-10-15";
 
     /**
      * The build date is updated for each public release.
      */
-    public static final String BUILD_DATE_STABLE = "2010-08-31";
+    public static final String BUILD_DATE_STABLE = "2010-09-18";
 
     /**
      * The TCP protocol version number 6.

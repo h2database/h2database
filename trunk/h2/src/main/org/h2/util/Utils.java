5649b0ef04e384d73002464ec224a0645b7738b8
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2010-10-21 18:36:47 +0000

    Remove unused code.

--- trunk/h2/src/main/org/h2/util/Utils.java
+++ trunk/h2/src/main/org/h2/util/Utils.java
@@ -93,19 +93,6 @@ public class Utils {
     }
 
     /**
-     * Write a long value to the byte array.
-     *
-     * @param buff the byte array
-     * @param pos the position
-     * @param x the value
-     */
-    public static void writeLong(byte[] buff, int pos, long x) {
-        for (int i = 0; i < 8; i++) {
-            buff[pos + i] = (byte) ((x >> (8 * (8 - i))) & 255);
-        }
-    }
-
-    /**
      * Calculate the index of the first occurrence of the pattern in the byte
      * array, starting with the given index. This methods returns -1 if the
      * pattern has not been found, and the start position if the pattern is

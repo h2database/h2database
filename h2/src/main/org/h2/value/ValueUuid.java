1114d8389cc199db3c4a44315fb3ff6341664381
Thomas Mueller <thomasmueller@users.noreply.github.com>
2010-11-20 15:13:48 +0000

    Converting binary data to UUID was incorrect when the converting more than 31 bytes. Now always the first 16 bytes are used.

--- h2/src/main/org/h2/value/ValueUuid.java
+++ h2/src/main/org/h2/value/ValueUuid.java
@@ -57,17 +57,17 @@ public class ValueUuid extends Value {
     }
 
     /**
-     * Get or create a UUID for the given 32 bytes.
+     * Get or create a UUID for the given 16 bytes.
      *
-     * @param binary the byte array (must be at least 32 bytes long)
+     * @param binary the byte array (must be at least 16 bytes long)
      * @return the UUID
      */
     public static ValueUuid get(byte[] binary) {
-        if (binary.length < 32) {
+        if (binary.length < 16) {
             return get(StringUtils.convertBytesToString(binary));
         }
         long high = Utils.readLong(binary, 0);
-        long low = Utils.readLong(binary, 16);
+        long low = Utils.readLong(binary, 8);
         return (ValueUuid) Value.cache(new ValueUuid(high, low));
     }
 

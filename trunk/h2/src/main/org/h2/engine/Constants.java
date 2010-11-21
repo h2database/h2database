166355c66ee65730c0764e4ec85a7fea80f53718
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2010-11-21 16:35:11 +0000

    Prepare release.

--- trunk/h2/src/main/org/h2/engine/Constants.java
+++ trunk/h2/src/main/org/h2/engine/Constants.java
@@ -16,7 +16,7 @@ public class Constants {
     /**
      * The build date is updated for each public release.
      */
-    public static final String BUILD_DATE = "2010-11-08";
+    public static final String BUILD_DATE = "2010-11-21";
 
     /**
      * The build date is updated for each public release.
@@ -26,7 +26,7 @@ public class Constants {
     /**
      * The build id is incremented for each public release.
      */
-    public static final int BUILD_ID = 146;
+    public static final int BUILD_ID = 147;
 
     /**
      * The build id of the last stable release.

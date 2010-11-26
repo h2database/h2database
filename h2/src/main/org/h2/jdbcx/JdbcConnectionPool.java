b903a3145658d35505cde25f3c65039c771f292c
Thomas Mueller <thomasmueller@users.noreply.github.com>
2010-11-26 19:25:21 +0000

    Connection pool / DataSource: a NullPointerException was thrown when using a database URL that doesn't start with "jdbc:h2:".

--- h2/src/main/org/h2/jdbcx/JdbcConnectionPool.java
+++ h2/src/main/org/h2/jdbcx/JdbcConnectionPool.java
@@ -196,7 +196,7 @@ public class JdbcConnectionPool implements DataSource, ConnectionEventListener {
      */
     public Connection getConnection() throws SQLException {
         long max = System.currentTimeMillis() + timeout * 1000;
-        while (System.currentTimeMillis() <= max) {
+        do {
             synchronized (this) {
                 if (activeConnections < maxConnections) {
                     return getConnectionNow();
@@ -207,7 +207,7 @@ public class JdbcConnectionPool implements DataSource, ConnectionEventListener {
                     // ignore
                 }
             }
-        }
+        } while (System.currentTimeMillis() <= max);
         throw new SQLException("Login timeout", "08001", 8001);
     }
 

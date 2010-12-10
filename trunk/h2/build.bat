b182fdc94619ab21710b7c24d1c066b9c705150f
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2010-12-10 08:47:43 +0000

    Improve build script

--- trunk/h2/build.bat
+++ trunk/h2/build.bat
@@ -1,6 +1,6 @@
 @echo off
 if "%JAVA_HOME%"=="" echo Error: JAVA_HOME is not defined.
-if "%1"=="clean" rmdir /s /q temp | rmdir /s /q bin
+if "%1"=="clean" rmdir /s /q temp bin 2>nul
 if not exist temp mkdir temp
 if not exist bin mkdir bin
 "%JAVA_HOME%/bin/javac" -sourcepath src/tools -d bin src/tools/org/h2/build/*.java

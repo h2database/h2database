36841fdfe08df3ef72f4bf9f6775680bc27e350a
Thomas Mueller <thomasmueller@users.noreply.github.com>
2010-12-12 11:37:25 +0000

    Prepare release.

--- h2/src/main/org/h2/engine/Constants.java
+++ h2/src/main/org/h2/engine/Constants.java
@@ -16,22 +16,22 @@ public class Constants {
     /**
      * The build date is updated for each public release.
      */
-    public static final String BUILD_DATE = "2010-11-21";
+    public static final String BUILD_DATE = "2010-12-12";
 
     /**
      * The build date is updated for each public release.
      */
-    public static final String BUILD_DATE_STABLE = "2010-11-02";
+    public static final String BUILD_DATE_STABLE = "2010-11-21";
 
     /**
      * The build id is incremented for each public release.
      */
-    public static final int BUILD_ID = 147;
+    public static final int BUILD_ID = 148;
 
     /**
      * The build id of the last stable release.
      */
-    public static final int BUILD_ID_STABLE = 145;
+    public static final int BUILD_ID_STABLE = 147;
 
     /**
      * If H2 is compiled to be included in a product, this should be set to

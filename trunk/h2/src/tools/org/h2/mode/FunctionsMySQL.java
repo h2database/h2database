dc4dbe8e48901984213bf52f3b864f2cf060f001
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2011-01-13 19:29:03 +0000

    Change copyright years.

--- trunk/h2/src/tools/org/h2/mode/FunctionsMySQL.java
+++ trunk/h2/src/tools/org/h2/mode/FunctionsMySQL.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2010 H2 Group. Multiple-Licensed under the H2 License,
+ * Copyright 2004-2011 H2 Group. Multiple-Licensed under the H2 License,
  * Version 1.0, and under the Eclipse Public License, Version 1.0
  * (http://h2database.com/html/license.html).
  * Initial Developer: Jason Brittain (jason.brittain at gmail.com)

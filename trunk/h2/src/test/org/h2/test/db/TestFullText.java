efabe4877a532e90b4473bb619f62e7cc6a3845b
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2011-05-20 10:17:56 +0000

    Native fulltext search: the characters '<', '>', and '\' are now also whitespace characters. Also, the list of whitespace characters can be changed using FullText.setWhitespaceChars(conn, ...)

--- trunk/h2/src/test/org/h2/test/db/TestFullText.java
+++ trunk/h2/src/test/org/h2/test/db/TestFullText.java
@@ -83,8 +83,9 @@ public class TestFullText extends TestBase {
         stat.execute("CREATE ALIAS IF NOT EXISTS FT_INIT FOR \"org.h2.fulltext.FullText.init\"");
         stat.execute("CALL FT_INIT()");
         FullText.setIgnoreList(conn, "to,this");
+        FullText.setWhitespaceChars(conn, " ,.-");
         stat.execute("CREATE TABLE TEST(ID INT PRIMARY KEY, NAME VARCHAR)");
-        stat.execute("INSERT INTO TEST VALUES(1, 'Welcome to this world')");
+        stat.execute("INSERT INTO TEST VALUES(1, 'Welcome to this world, One_Word')");
         stat.execute("CALL FT_CREATE_INDEX('PUBLIC', 'TEST', NULL)");
         ResultSet rs;
         rs = stat.executeQuery("SELECT * FROM FT_SEARCH('Welcome', 0, 0)");
@@ -93,6 +94,10 @@ public class TestFullText extends TestBase {
         assertEquals("SCORE", rs.getMetaData().getColumnLabel(2));
         assertEquals("\"PUBLIC\".\"TEST\" WHERE \"ID\"=1", rs.getString(1));
         assertEquals("1.0", rs.getString(2));
+        rs = stat.executeQuery("SELECT * FROM FT_SEARCH_DATA('One', 0, 0)");
+        assertFalse(rs.next());
+        rs = stat.executeQuery("SELECT * FROM FT_SEARCH_DATA('One_Word', 0, 0)");
+        assertTrue(rs.next());
         rs = stat.executeQuery("SELECT * FROM FT_SEARCH_DATA('Welcome', 0, 0)");
         assertTrue(rs.next());
         assertEquals("SCHEMA", rs.getMetaData().getColumnLabel(1));

2152280aedbb8c2016de356a9b12bf90021053f8
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2011-06-23 04:19:53 +0000

    The optimization for COUNT(..) on columns that are not nullable was also used for COUNT(DISTINCT ..), which is incorrect.

--- trunk/h2/src/test/org/h2/test/db/TestCases.java
+++ trunk/h2/src/test/org/h2/test/db/TestCases.java
@@ -37,6 +37,7 @@ public class TestCases extends TestBase {
     }
 
     public void test() throws Exception {
+        testCountDistinctNotNull();
         testDependencies();
         testConvertType();
         testSortedSelect();
@@ -91,6 +92,17 @@ public class TestCases extends TestBase {
         testConstraintReconnect();
         testCollation();
         deleteDb("cases");
+    }
+
+    private void testCountDistinctNotNull() throws SQLException {
+        deleteDb("cases");
+        Connection conn = getConnection("cases");
+        Statement stat = conn.createStatement();
+        stat.execute("create table test(id int not null) as select 1 from system_range(1, 10)");
+        ResultSet rs = stat.executeQuery("select count(distinct id) from test");
+        rs.next();
+        assertEquals(1, rs.getInt(1));
+        conn.close();
     }
 
     private void testDependencies() throws SQLException {

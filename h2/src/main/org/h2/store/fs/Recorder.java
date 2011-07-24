7b61f73c83fc312200973641277fc495037adccb
Thomas Mueller <thomasmueller@users.noreply.github.com>
2011-07-24 15:22:16 +0000

    To test recovery, append ;RECOVER_TEST=64 to the database URL.

--- h2/src/main/org/h2/store/fs/Recorder.java
+++ h2/src/main/org/h2/store/fs/Recorder.java
@@ -4,7 +4,7 @@
  * (http://h2database.com/html/license.html).
  * Initial Developer: H2 Group
  */
-package org.h2.test.utils;
+package org.h2.store.fs;
 
 /**
  * A recorder for the recording file system.

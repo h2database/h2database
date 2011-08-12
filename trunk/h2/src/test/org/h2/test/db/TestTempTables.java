9cc0bd43e8034cba6b26b384229ab362acce1a8a
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2011-08-12 10:54:29 +0000

    Creating a temporary table with the option 'transactional' will now also create the indexes in transactional mode

--- trunk/h2/src/test/org/h2/test/db/TestTempTables.java
+++ trunk/h2/src/test/org/h2/test/db/TestTempTables.java
@@ -143,8 +143,8 @@ public class TestTempTables extends TestBase {
         stat.execute("insert into test values(1)");
         stat.execute("commit");
         stat.execute("insert into test values(2)");
-        stat.execute("create local temporary table temp(id int primary key) transactional");
-        stat.execute("insert into temp values(3)");
+        stat.execute("create local temporary table temp(id int primary key, name varchar, constraint x index(name)) transactional");
+        stat.execute("insert into temp values(3, 'test')");
         stat.execute("rollback");
         rs = stat.executeQuery("select * from test");
         assertTrue(rs.next());

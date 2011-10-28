5a1cbb40121f6ccb9d279114184041e9f2154940
Thomas Mueller <thomasmueller@users.noreply.github.com>
2011-10-28 14:17:51 +0000

    Documentation.

--- h2/src/main/org/h2/util/DateTimeUtils.java
+++ h2/src/main/org/h2/util/DateTimeUtils.java
@@ -37,7 +37,7 @@ public class DateTimeUtils {
      */
     public static final long MILLIS_PER_DAY = 24 * 60 * 60 * 1000L;
 
-    public static final long NANOS_PER_DAY = MILLIS_PER_DAY * 1000000;
+    private static final long NANOS_PER_DAY = MILLIS_PER_DAY * 1000000;
 
     private static final int SHIFT_YEAR = 9;
     private static final int SHIFT_MONTH = 5;

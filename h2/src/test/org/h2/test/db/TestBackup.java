d3fee38722a30043bfcc90d8ddbe9a16b0e2c057
Thomas Mueller <thomasmueller@users.noreply.github.com>
2012-01-30 18:18:27 +0000

    Multi-threaded kernel: concurrently running an online backup and updating the database resulted in a broken (transactionally incorrect) backup file in some cases.

--- h2/src/test/org/h2/test/db/TestBackup.java
+++ h2/src/test/org/h2/test/db/TestBackup.java
@@ -7,13 +7,17 @@
 package org.h2.test.db;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.concurrent.atomic.AtomicLong;
 
+import org.h2.api.DatabaseEventListener;
 import org.h2.store.fs.FileUtils;
 import org.h2.test.TestBase;
 import org.h2.tools.Backup;
 import org.h2.tools.Restore;
+import org.h2.util.Task;
 
 /**
  * Test for the BACKUP SQL statement.
@@ -33,11 +37,101 @@ public class TestBackup extends TestBase {
         if (config.memory) {
             return;
         }
+        testConcurrentBackup();
         testBackupRestoreLobStatement();
         testBackupRestoreLob();
         testBackup();
         deleteDb("backup");
         FileUtils.delete(getBaseDir() + "/backup.zip");
+    }
+
+    private void testConcurrentBackup() throws SQLException {
+        if (config.networked || !config.big) {
+            return;
+        }
+        deleteDb("backup");
+        String url = getURL("backup;multi_threaded=true", true);
+        Connection conn = getConnection(url);
+        final Statement stat = conn.createStatement();
+        stat.execute("create table test(id int primary key, name varchar)");
+        stat.execute("insert into test select x, 'Hello' from system_range(1, 2)");
+        conn.setAutoCommit(false);
+        Connection conn1;
+        conn1 = getConnection(url);
+        final AtomicLong updateEnd = new AtomicLong();
+        final Statement stat1 = conn.createStatement();
+        Task task = new Task() {
+            public void call() throws Exception {
+                while (!stop) {
+                    if (System.currentTimeMillis() < updateEnd.get()) {
+                        stat.execute("update test set name = 'Hallo'");
+                        stat1.execute("checkpoint");
+                        stat.execute("update test set name = 'Hello'");
+                        stat.execute("commit");
+                        stat.execute("checkpoint");
+                    } else {
+                        Thread.sleep(10);
+                    }
+                }
+            }
+        };
+        Connection conn2;
+        conn2 = getConnection(url + ";database_event_listener='" + BackupListener.class.getName() + "'");
+        Statement stat2 = conn2.createStatement();
+        task.execute();
+        for (int i = 0; i < 10; i++) {
+            updateEnd.set(System.currentTimeMillis() + 2000);
+            stat2.execute("backup to '"+getBaseDir()+"/backup.zip'");
+            stat2.execute("checkpoint");
+            Restore.execute(getBaseDir() + "/backup.zip", getBaseDir() + "/t2", "backup", true);
+            Connection conn3;
+            conn3 = getConnection("t2/backup");
+            Statement stat3 = conn3.createStatement();
+            stat3.execute("script");
+            ResultSet rs = stat3.executeQuery("select * from test where name='Hallo'");
+            while (rs.next()) {
+                fail();
+            }
+            conn3.close();
+        }
+        task.get();
+        conn.close();
+        conn1.close();
+        conn2.close();
+    }
+
+    /**
+     * A backup listener to test concurrent backup.
+     */
+    public static class BackupListener implements DatabaseEventListener {
+
+        public void closingDatabase() {
+            // ignore
+        }
+
+        public void exceptionThrown(SQLException e, String sql) {
+            // ignore
+        }
+
+        public void init(String url) {
+            // ignore
+        }
+
+        public void opened() {
+            // ignore
+        }
+
+        public void setProgress(int state, String name, int x, int max) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                // ignore
+            }
+            if (x % 400 == 0) {
+                // System.out.println("state: " + state + " name: " + name + " x:" + x + "/" + max);
+            }
+        }
+
     }
 
     private void testBackupRestoreLob() throws SQLException {

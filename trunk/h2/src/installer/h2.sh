a64f3870bdedd70ae3d498440bf11b2acf87d0bd
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2012-03-02 14:02:35 +0000

    The shell script h2.sh did not work with spaces in the path. It also works now with quoted spaces in the argument list.

--- trunk/h2/src/installer/h2.sh
+++ trunk/h2/src/installer/h2.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-dir="$(dirname $0)"
-cp="$dir/h2.jar"
-if [ -n "$H2DRIVERS" ] ; then
-  cp="$cp:$H2DRIVERS"
-fi
-if [ -n "$CLASSPATH" ] ; then
-  cp="$cp:$CLASSPATH"
-fi
-java -cp "$cp" org.h2.tools.Console $@
-
+dir=$(dirname "$0")
+java -cp "$dir/h2.jar:$H2DRIVERS:$CLASSPATH" org.h2.tools.Console "$@"

8bb2b2c81d5351f50cd2cd17bae95e861f7eea31
thomas.tom.mueller <thomas.tom.mueller@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2012-06-26 06:49:30 +0000

    Documentation.

--- trunk/h2/src/main/org/h2/security/XTEA.java
+++ trunk/h2/src/main/org/h2/security/XTEA.java
@@ -11,11 +11,12 @@ import org.h2.message.DbException;
 
 /**
  * An implementation of the XTEA block cipher algorithm.
+ * <p>
  * This implementation uses 32 rounds.
+ * The best attack reported as of 2009 is 36 rounds (Wikipedia).
  */
 public class XTEA implements BlockCipher {
 
-    // best attack reported as of 2004 is 26 rounds [wikipedia]
     private static final int DELTA = 0x9E3779B9;
     private int k0, k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11, k12, k13, k14, k15;
     private int k16, k17, k18, k19, k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k30, k31;

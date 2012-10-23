d657686e7e18a5b44d0e8eb3fb948a7fa246ae0e
thomas.tom.mueller@gmail.com <thomas.tom.mueller@gmail.com@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2012-10-23 18:24:29 +0000

    Make the encryption classes usable by tools.

--- trunk/h2/src/main/org/h2/security/XTEA.java
+++ trunk/h2/src/main/org/h2/security/XTEA.java
@@ -21,10 +21,6 @@ public class XTEA implements BlockCipher {
     private int k0, k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11, k12, k13, k14, k15;
     private int k16, k17, k18, k19, k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k30, k31;
 
-    XTEA() {
-        // do nothing
-    }
-
     public void setKey(byte[] b) {
         int[] key = new int[4];
         for (int i = 0; i < 16;) {

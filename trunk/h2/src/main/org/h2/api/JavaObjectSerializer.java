b41a455fc85b612b6d5c8000ebc0f6600619457c
Sergi.Vladykin@gmail.com <Sergi.Vladykin@gmail.com@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2012-11-24 09:28:11 +0000

    pluggable java object serialization mechanism added

---
+++ trunk/h2/src/main/org/h2/api/JavaObjectSerializer.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2004-2011 H2 Group. Multiple-Licensed under the H2 License,
+ * Version 1.0, and under the Eclipse Public License, Version 1.0
+ * (http://h2database.com/html/license.html).
+ * Initial Developer: H2 Group
+ */
+package org.h2.api;
+
+/**
+ * Custom serialization mechanism for java objects being stored in column of type OTHER.
+ *
+ * @author Sergi Vladykin
+ */
+public interface JavaObjectSerializer {
+
+    /**
+     * Serialize object to byte array.
+     *
+     * @param obj
+     * @return
+     */
+    byte[] serialize(Object obj) throws Exception;
+
+    /**
+     * Deserialize object from byte array.
+     *
+     * @param bytes
+     * @return
+     */
+    Object deserialize(byte[] bytes) throws Exception;
+
+}

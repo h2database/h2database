b91026abd4bf0f18e63145f8aabe191d2332a509
Sergi.Vladykin@gmail.com <Sergi.Vladykin@gmail.com@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2013-04-03 22:26:52 +0000

    fixed failure on incorrect limit and offsets with sorting, need to add tests

--- trunk/h2/src/main/org/h2/result/SortOrder.java
+++ trunk/h2/src/main/org/h2/result/SortOrder.java
@@ -163,16 +163,23 @@ public class SortOrder implements Comparator<Value[]> {
      * @param limit the limit
      */
     public void sort(ArrayList<Value[]> rows, int offset, int limit) {
-        if (rows.isEmpty()) {
+        int rowsSize = rows.size();
+    	if (rows.isEmpty() || offset >= rowsSize || limit == 0) {
             return;
+        }
+        if (offset < 0) {
+            offset = 0;
+        }
+        if (offset + limit > rowsSize) {
+            limit = rowsSize - offset;
         }
         if (limit == 1 && offset == 0) {
             rows.set(0, Collections.min(rows, this));
             return;
         }
-        Value[][] arr = rows.toArray(new Value[rows.size()][]);
+        Value[][] arr = rows.toArray(new Value[rowsSize][]);
         Utils.sortTopN(arr, offset, limit, this);
-        for (int i = 0, end = Math.min(offset + limit, arr.length); i < end; i++) {
+        for (int i = 0, end = Math.min(offset + limit, rowsSize); i < end; i++) {
             rows.set(i, arr[i]);
         }
     }

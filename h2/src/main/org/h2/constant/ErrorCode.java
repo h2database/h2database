c26fc5924cf26a3dec4ec5efae96c7e910c5aab7
Thomas Mueller <thomasmueller@users.noreply.github.com>
2013-04-16 20:32:31 +0000

    Require Java 6

--- h2/src/main/org/h2/constant/ErrorCode.java
+++ h2/src/main/org/h2/constant/ErrorCode.java
@@ -1357,8 +1357,6 @@ public class ErrorCode {
      * The error with code <code>90092</code> is thrown when
      * the source code is not compiled for the Java platform used.
      * At runtime, the existence of the class java.sql.Savepoint is checked.
-     * To run this database in JDK 1.3, it is first required to switch the
-     * source code to JDK 1.3 using ant switchSourceJdk13.
      */
     public static final int UNSUPPORTED_JAVA_VERSION = 90092;
 

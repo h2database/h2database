d96cca6d3e5c79356b06b3ec81391a3b4c07d0c4
thomas.tom.mueller@gmail.com <thomas.tom.mueller@gmail.com@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2013-05-10 15:27:56 +0000

    Cleanup

--- trunk/h2/src/test/org/h2/test/unit/TestValueMemory.java
+++ trunk/h2/src/test/org/h2/test/unit/TestValueMemory.java
@@ -17,7 +17,6 @@ import java.util.Random;
 import org.h2.engine.Constants;
 import org.h2.store.DataHandler;
 import org.h2.store.FileStore;
-import org.h2.store.LobStorageBackend;
 import org.h2.store.LobStorageFrontend;
 import org.h2.test.TestBase;
 import org.h2.test.utils.MemoryFootprint;

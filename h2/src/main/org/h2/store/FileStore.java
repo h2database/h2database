c4a0d51fbf1627ae7f942841efe392d3f1300599
noelgrandin <noelgrandin@gmail.com>
2013-06-11 13:57:37 +0000

    Fix potential UTF8 encoding issue in org.h2.store.FileStore, reported by Juerg Spiess

--- h2/src/main/org/h2/store/FileStore.java
+++ h2/src/main/org/h2/store/FileStore.java
@@ -135,7 +135,7 @@ public class FileStore {
      * @return the random salt or the magic
      */
     protected byte[] generateSalt() {
-        return HEADER.getBytes();
+        return HEADER.getBytes(Constants.UTF8);
     }
 
     /**
@@ -170,7 +170,7 @@ public class FileStore {
     public void init() {
         int len = Constants.FILE_BLOCK_SIZE;
         byte[] salt;
-        byte[] magic = HEADER.getBytes();
+        byte[] magic = HEADER.getBytes(Constants.UTF8);
         if (length() < HEADER_LENGTH) {
             // write unencrypted
             checkedWriting = false;

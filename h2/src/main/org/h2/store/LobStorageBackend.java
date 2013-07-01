e7c582386cfd22beee84378d1d077117f72ee487
noelgrandin <noelgrandin@gmail.com>
2013-07-01 07:05:13 +0000

    fix: I should have been using remainingBytes, not length.

--- h2/src/main/org/h2/store/LobStorageBackend.java
+++ h2/src/main/org/h2/store/LobStorageBackend.java
@@ -616,7 +616,7 @@ public class LobStorageBackend implements LobStorageInterface {
 
         public LobInputStream(long lob, long byteCount) {
             this.lob = lob;
-            remainingBytes = byteCount;
+            this.remainingBytes = byteCount;
             this.length = byteCount;
         }
 
@@ -670,10 +670,10 @@ public class LobStorageBackend implements LobStorageInterface {
 
         @Override
         public int available() throws IOException {
-            if (length > Integer.MAX_VALUE) {
+            if (remainingBytes > Integer.MAX_VALUE) {
                 return Integer.MAX_VALUE;
             }
-            return MathUtils.convertLongToInt(length);
+            return MathUtils.convertLongToInt(remainingBytes);
         }
 
         @Override

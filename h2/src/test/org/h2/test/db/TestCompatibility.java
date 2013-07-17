47551a41e85ec9672c2af2754d4234564e608a71
noelgrandin <noelgrandin@gmail.com>
2013-07-17 06:44:57 +0000

    Issue 499: support MySQL "UNIQUE KEY (ID) USING BTREE" constraint syntax

--- h2/src/test/org/h2/test/db/TestCompatibility.java
+++ h2/src/test/org/h2/test/db/TestCompatibility.java
@@ -267,6 +267,7 @@ public class TestCompatibility extends TestBase {
         stat.execute("CREATE TABLE TEST_5(ID INT PRIMARY KEY) ENGINE=InnoDb auto_increment=3 default charset=UTF8");
         stat.execute("CREATE TABLE TEST_6(ID INT PRIMARY KEY) ENGINE=InnoDb auto_increment=3 charset=UTF8");
         stat.execute("CREATE TABLE TEST_7(ID INT, KEY TEST_7_IDX(ID) USING BTREE)");
+        stat.execute("CREATE TABLE TEST_8(ID INT, UNIQUE KEY TEST_8_IDX(ID) USING BTREE)");
 
         // this maps to SET REFERENTIAL_INTEGRITY TRUE/FALSE
         stat.execute("SET foreign_key_checks = 0");

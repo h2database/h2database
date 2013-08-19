c1844be3e2c00a18b7745d7f256cb061e07ebd10
noelgrandin <noelgrandin@gmail.com>
2013-08-19 08:29:26 +0000

    Add an UNDO_LOG_SIZE column to the SESSIONS metadata table, to allow detecting when rogue sessions are creating large transactions.

--- h2/src/main/org/h2/table/MetaTable.java
+++ h2/src/main/org/h2/table/MetaTable.java
@@ -488,7 +488,8 @@ public class MetaTable extends Table {
                     "USER_NAME",
                     "SESSION_START",
                     "STATEMENT",
-                    "STATEMENT_START"
+                    "STATEMENT_START",
+                    "UNDO_LOG_SIZE"
             );
             break;
         }
@@ -1547,7 +1548,9 @@ public class MetaTable extends Table {
                             // STATEMENT
                             command == null ? null : command.toString(),
                             // STATEMENT_START
-                            new Timestamp(start).toString()
+                            new Timestamp(start).toString(),
+                            // UNDO_LOG_SIZE
+                            "" + s.getUndoLogSize()
                     );
                 }
             }

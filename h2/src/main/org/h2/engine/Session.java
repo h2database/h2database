3af6bbcab666f9e768965b866387319b9c2ace8e
Thomas Mueller <thomasmueller@users.noreply.github.com>
2013-09-29 15:56:40 +0000

    MVTableEngine: test

--- h2/src/main/org/h2/engine/Session.java
+++ h2/src/main/org/h2/engine/Session.java
@@ -1382,7 +1382,7 @@ public class Session extends SessionWithState {
     public Transaction getTransaction() {
         if (transaction == null) {
             if (database.getMvStore().getStore().isClosed()) {
-                throw DbException.get(ErrorCode.OBJECT_CLOSED);
+                throw DbException.get(ErrorCode.DATABASE_IS_CLOSED);
             }
             transaction = database.getMvStore().getTransactionStore().begin();
             startStatement = -1;

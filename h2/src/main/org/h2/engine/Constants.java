d6e6a5672899bd92cc0c2b6abbcfae454f7f9c01
Thomas Mueller <thomasmueller@users.noreply.github.com>
2014-04-04 17:19:15 +0000

    Switch back to 1.3

--- h2/src/main/org/h2/engine/Constants.java
+++ h2/src/main/org/h2/engine/Constants.java
@@ -100,7 +100,7 @@ public class Constants {
     /**
      * The minor version of this database.
      */
-    public static final int VERSION_MINOR = 4;
+    public static final int VERSION_MINOR = 3;
 
     /**
      * The lock mode that means no locking is used at all.

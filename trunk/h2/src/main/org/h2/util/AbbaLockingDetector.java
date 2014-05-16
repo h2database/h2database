7005303eeba98984205a80f6ff22953afac2c816
noelgrandin <noelgrandin@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2014-05-16 08:50:27 +0000

    improvements to AbbaLockingDetector

--- trunk/h2/src/main/org/h2/util/AbbaLockingDetector.java
+++ trunk/h2/src/main/org/h2/util/AbbaLockingDetector.java
@@ -10,46 +10,29 @@ import java.lang.management.MonitorInfo;
 import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.WeakHashMap;
 
 /**
- * A simple CPU profiling tool similar to java -Xrunhprof. It can be used
- * in-process (to profile the current application) or as a standalone program
- * (to profile a different process, or files containing full thread dumps).
+ * Utility to detect AB-BA deadlocks.
  */
 public class AbbaLockingDetector implements Runnable {
 
     private int tickInterval_ms = 2;
     private volatile boolean stop;
-    private final Set<String> lockObjectNames = new HashSet<String>();
 
-    /**
-     * Necessary because String[] does not implement a deep equals and hashcode.
-     */
-    private static final class LockOrder {
-        public final String[] names;
-
-        public LockOrder(String[] names) {
-            this.names = names;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            return Arrays.deepEquals(names, ((LockOrder) obj).names);
-        }
-
-        @Override
-        public int hashCode() {
-            return Arrays.deepHashCode(names);
-        }
-    }
-
-    private final Set<LockOrder> lockObjectOrders = new HashSet<LockOrder>();
     private final ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
     private Thread thread;
+
+    /**
+     * Map of (object A) -> ( map of (object locked before object A) ->
+     * (stacktrace where locked) )
+     */
+    private final Map<String, Map<String, String>> LOCK_ORDERING = new WeakHashMap<String, Map<String, String>>();
+    private final Set<String> KNOWN_DEADLOCKS = new HashSet<String>();
 
     /**
      * Start collecting locking data.
@@ -101,44 +84,149 @@ public class AbbaLockingDetector implements Runnable {
             }
         }
 
-        ThreadInfo[] list = threadMXBean.dumpAllThreads(true/* lockedMonitors */, true/* lockedSynchronizers */);
-        processList(list);
+        ThreadInfo[] list = threadMXBean.dumpAllThreads(true/* lockedMonitors */, false/* lockedSynchronizers */);
+        processThreadList(list);
     }
 
-    private void processList(ThreadInfo[] threadInfoList) {
-        List<String> lockOrder = new ArrayList<String>();
+    private void processThreadList(ThreadInfo[] threadInfoList) {
+        final List<String> lockOrder = new ArrayList<String>();
         for (ThreadInfo threadInfo : threadInfoList) {
-            MonitorInfo[] monitorInfoList = threadInfo.getLockedMonitors();
             lockOrder.clear();
-            for (MonitorInfo monitorInfo : monitorInfoList) {
-                String lockName = monitorInfo.getClassName();
-                if (lockName.startsWith("org.h2")) {
-                    lockObjectNames.add(lockName);
-                    // ignore locks which are locked multiple times in succession - Java locks are recursive
-                    if (lockOrder.isEmpty() || !lockOrder.get(lockOrder.size() - 1).equals(lockName)) {
+            generateOrdering(lockOrder, threadInfo);
+            if (lockOrder.size() > 1) {
+                markHigher(lockOrder, threadInfo);
+            }
+        }
+    }
+
+    /**
+     * We cannot simply call getLockedMonitors because it is not guaranteed to
+     * return the locks in the correct order.
+     */
+    private static void generateOrdering(final List<String> lockOrder, ThreadInfo info) {
+        final MonitorInfo[] lockedMonitors = info.getLockedMonitors();
+        final StackTraceElement[] stackTrace = info.getStackTrace();
+        for (int i = stackTrace.length - 1; i >= 0; i--) {
+            for (MonitorInfo mi : lockedMonitors) {
+                if (mi.getLockedStackDepth() == i) {
+                    String lockName = getObjectName(mi);
+                    if (lockName.equals("sun.misc.Launcher$AppClassLoader")) {
+                        // ignore, it shows up everywhere
+                        continue;
+                    }
+                    // Ignore locks which are locked multiple times in
+                    // succession - Java locks are recursive.
+                    if (!lockOrder.contains(lockName)) {
                         lockOrder.add(lockName);
                     }
                 }
             }
-            if (lockOrder.size()>1) {
-                lockObjectOrders.add(new LockOrder(lockOrder.toArray(new String[lockOrder.size()])));
+        }
+    }
+
+    private void markHigher(List<String> lockOrder, ThreadInfo threadInfo) {
+        String topLock = lockOrder.get(lockOrder.size() - 1);
+        Map<String, String> map = LOCK_ORDERING.get(topLock);
+        if (map == null) {
+            map = new WeakHashMap<String, String>();
+            LOCK_ORDERING.put(topLock, map);
+        }
+        String oldException = null;
+        for (int i = 0; i < lockOrder.size() - 1; i++) {
+            String olderLock = lockOrder.get(i);
+            Map<String, String> oldMap = LOCK_ORDERING.get(olderLock);
+            boolean foundDeadLock = false;
+            if (oldMap != null) {
+                String e = oldMap.get(topLock);
+                if (e != null) {
+                    foundDeadLock = true;
+                    String deadlockType = topLock + " " + olderLock;
+                    if (!KNOWN_DEADLOCKS.contains(deadlockType)) {
+                        System.out.println(topLock + " synchronized after \n " + olderLock
+                                + ", but in the past before\n" + "AFTER\n" + getStackTraceForThread(threadInfo)
+                                + "BEFORE\n" + e);
+                        KNOWN_DEADLOCKS.add(deadlockType);
+                    }
+                }
+            }
+            if (!foundDeadLock && !map.containsKey(olderLock)) {
+                if (oldException == null) {
+                    oldException = getStackTraceForThread(threadInfo);
+                }
+                map.put(olderLock, oldException);
             }
         }
     }
 
-    public void dump() {
-        stopCollecting();
-        System.out.println("Locked Objects");
-        System.out.println("--------------------------------");
-        for (String s : lockObjectNames) {
-            System.out.println(s);
+    /**
+     * Dump data in the same format as {@link ThreadInfo#toString()}, but with
+     * some modifications (no stack frame limit, and removal of uninteresting
+     * stack frames)
+     */
+    private static String getStackTraceForThread(ThreadInfo info) {
+        StringBuilder sb = new StringBuilder("\"" + info.getThreadName() + "\"" + " Id=" + info.getThreadId() + " "
+                + info.getThreadState());
+        if (info.getLockName() != null) {
+            sb.append(" on " + info.getLockName());
         }
-        System.out.println();
-        System.out.println("Locking Orders");
-        System.out.println("--------------------------------");
-        for (LockOrder s : lockObjectOrders) {
-            System.out.println(Arrays.toString(s.names));
+        if (info.getLockOwnerName() != null) {
+            sb.append(" owned by \"" + info.getLockOwnerName() + "\" Id=" + info.getLockOwnerId());
         }
+        if (info.isSuspended()) {
+            sb.append(" (suspended)");
+        }
+        if (info.isInNative()) {
+            sb.append(" (in native)");
+        }
+        sb.append('\n');
+        StackTraceElement[] stackTrace = info.getStackTrace();
+        for (int i = 0; i < stackTrace.length; i++) {
+            boolean dumpedStackTraceElement = false;
+            StackTraceElement ste = stackTrace[i];
+
+            MonitorInfo[] lockedMonitors = info.getLockedMonitors();
+            for (MonitorInfo mi : lockedMonitors) {
+                if (mi.getLockedStackDepth() == i) {
+                    // Only start dumping the stack from the first time we lock
+                    // something.
+                    // Removes a lot of unnecessary noise from the output.
+                    if (!dumpedStackTraceElement) {
+                        dumpStackTraceElement(info, sb, i, ste);
+                        dumpedStackTraceElement = true;
+                    }
+                    sb.append("\t-  locked " + mi);
+                    sb.append('\n');
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    private static void dumpStackTraceElement(ThreadInfo info, StringBuilder sb, int i, StackTraceElement ste) {
+        sb.append("\tat " + ste.toString());
+        sb.append('\n');
+        if (i == 0 && info.getLockInfo() != null) {
+            Thread.State ts = info.getThreadState();
+            switch (ts) {
+            case BLOCKED:
+                sb.append("\t-  blocked on " + info.getLockInfo());
+                sb.append('\n');
+                break;
+            case WAITING:
+                sb.append("\t-  waiting on " + info.getLockInfo());
+                sb.append('\n');
+                break;
+            case TIMED_WAITING:
+                sb.append("\t-  waiting on " + info.getLockInfo());
+                sb.append('\n');
+                break;
+            default:
+            }
+        }
+    }
+
+    private static String getObjectName(MonitorInfo info) {
+        return info.getClassName();// + "@" + info.getIdentityHashCode();
     }
 
 }

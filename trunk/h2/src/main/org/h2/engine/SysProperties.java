4dabb7ca4499b00f0a08f29d99f91cfd82d8ae0e
noelgrandin@gmail.com <noelgrandin@gmail.com@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2014-06-12 13:29:56 +0000

    remove more dead code

--- trunk/h2/src/main/org/h2/engine/SysProperties.java
+++ trunk/h2/src/main/org/h2/engine/SysProperties.java
@@ -94,7 +94,7 @@ public class SysProperties {
      * to use Safari, use:
      * <code>java -Dh2.browser="open,-a,Safari,%url" ...</code>.
      */
-    public static final String BROWSER1 =
+    public static final String BROWSER =
             Utils.getProperty(H2_BROWSER, null);
 
     /**

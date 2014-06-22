142e696e12b795d1dde1fffedfa7d56e50c30b56
Thomas Mueller <thomasmueller@users.noreply.github.com>
2014-06-22 19:18:11 +0000

    License change to MPL 2.0 / EPL 1.0

--- h2/src/main/org/h2/security/CipherFactory.java
+++ h2/src/main/org/h2/security/CipherFactory.java
@@ -1,7 +1,6 @@
 /*
- * Copyright 2004-2013 H2 Group. Multiple-Licensed under the H2 License,
- * Version 1.0, and under the Eclipse Public License, Version 1.0
- * (http://h2database.com/html/license.html).
+ * Copyright 2004-2014 H2 Group. Multiple-Licensed under the MPL 2.0,
+ * and the EPL 1.0 (http://h2database.com/html/license.html).
  * Initial Developer: H2 Group
  */
 package org.h2.security;

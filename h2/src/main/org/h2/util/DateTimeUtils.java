bd3d964f0572aaf7b9bcf588552b6bed90df7c41
Thomas Mueller <thomasmueller@users.noreply.github.com>
2014-06-22 19:20:54 +0000

    License change to MPL 2.0 / EPL 1.0

--- h2/src/main/org/h2/util/DateTimeUtils.java
+++ h2/src/main/org/h2/util/DateTimeUtils.java
@@ -1,7 +1,6 @@
 /*
- * Copyright 2004-2013 H2 Group. Multiple-Licensed under the H2 License,
- * Version 1.0, and under the Eclipse Public License, Version 1.0
- * (http://h2database.com/html/license.html).
+ * Copyright 2004-2014 H2 Group. Multiple-Licensed under the MPL 2.0,
+ * and the EPL 1.0 (http://h2database.com/html/license.html).
  * Initial Developer: H2 Group
  * Iso8601:
  * Initial Developer: Robert Rathsack (firstName dot lastName at gmx dot de)
@@ -52,7 +51,7 @@ public class DateTimeUtils {
     private static final int[] DAYS_OFFSET = { 0, 31, 61, 92, 122, 153, 184,
             214, 245, 275, 306, 337, 366 };
 
-    private static final ThreadLocal<Calendar> CACHED_CALENDAR = 
+    private static final ThreadLocal<Calendar> CACHED_CALENDAR =
             new ThreadLocal<Calendar>() {
         @Override
         protected Calendar initialValue() {

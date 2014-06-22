ed255e11e889957a7d768344779d13d1f8799fcc
Thomas Mueller <thomasmueller@users.noreply.github.com>
2014-06-22 18:58:01 +0000

    License change to MPL 2.0 / EPL 1.0

--- h2/src/main/org/h2/compress/CompressLZF.java
+++ h2/src/main/org/h2/compress/CompressLZF.java
@@ -1,7 +1,6 @@
 /*
- * Copyright 2004-2013 H2 Group. Multiple-Licensed under the H2 License,
- * Version 1.0, and under the Eclipse Public License, Version 1.0
- * (http://h2database.com/html/license.html).
+ * Copyright 2004-2014 H2 Group. Multiple-Licensed under the MPL 2.0,
+ * and the EPL 1.0 (http://h2database.com/html/license.html).
  *
  * This code is based on the LZF algorithm from Marc Lehmann. It is a
  * re-implementation of the C code:

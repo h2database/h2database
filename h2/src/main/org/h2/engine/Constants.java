0c8ca6e6a5b0c49ffc694550fa8f6a2914878b5c
Thomas Mueller <thomasmueller@users.noreply.github.com>
2014-10-17 11:43:16 +0000

    Prepare release

--- h2/src/main/org/h2/engine/Constants.java
+++ h2/src/main/org/h2/engine/Constants.java
@@ -16,7 +16,7 @@ public class Constants {
     /**
      * The build date is updated for each public release.
      */
-    public static final String BUILD_DATE = "2014-08-06";
+    public static final String BUILD_DATE = "2014-10-17";
 
     /**
      * The build date of the last stable release.
@@ -26,7 +26,7 @@ public class Constants {
     /**
      * The build id is incremented for each public release.
      */
-    public static final int BUILD_ID = 181;
+    public static final int BUILD_ID = 182;
 
     /**
      * The build id of the last stable release.

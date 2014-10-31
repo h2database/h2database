c2f830ea47d73c84b50af98304fee121f9dde3cc
thomas.tom.mueller@gmail.com <thomas.tom.mueller@gmail.com@e6896862-9d19-0410-bfb3-e9fa5d50e656>
2014-10-31 10:03:37 +0000

    Close the file

--- trunk/h2/src/main/org/h2/expression/Function.java
+++ trunk/h2/src/main/org/h2/expression/Function.java
@@ -2544,6 +2544,7 @@ public class Function extends Expression implements FunctionCall {
             } catch (SQLException e) {
                 throw DbException.convert(e);
             } finally {
+                csv.close();
                 JdbcUtils.closeSilently(rs);
             }
             return x;

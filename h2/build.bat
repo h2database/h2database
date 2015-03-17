a021f586a1875752dd70fd8cc9922616afe30e3a
noelgrandin@gmail.com <noelgrandin@gmail.com>
2015-03-17 10:13:05 +0000

    default to using the system proxy settings

--- h2/build.bat
+++ h2/build.bat
@@ -4,4 +4,4 @@ if "%1"=="clean" rmdir /s /q temp bin 2>nul
 if not exist temp mkdir temp
 if not exist bin mkdir bin
 "%JAVA_HOME%/bin/javac" -sourcepath src/tools -d bin src/tools/org/h2/build/*.java
-"%JAVA_HOME%/bin/java" -Xmx256m -cp "bin;%JAVA_HOME%/lib/tools.jar;temp" org.h2.build.Build %*
+"%JAVA_HOME%/bin/java" -Djava.net.useSystemProxies=true -Xmx256m -cp "bin;%JAVA_HOME%/lib/tools.jar;temp" org.h2.build.Build %*

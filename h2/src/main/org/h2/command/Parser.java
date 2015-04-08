0a8c8b08e2c7174a241ade297f499b0d36eae0c3
noelgrandin@gmail.com <noelgrandin@gmail.com>
2015-04-08 08:41:35 +0000

    Add support for PostgreSQL STRING_AGG function. Patch by Fred Aquiles.

--- h2/src/main/org/h2/command/Parser.java
+++ h2/src/main/org/h2/command/Parser.java
@@ -2270,7 +2270,7 @@ public class Parser {
         }
     }
 
-    private Expression readAggregate(int aggregateType) {
+    private Expression readAggregate(int aggregateType, String aggregateName) {
         if (currentSelect == null) {
             throw getSyntaxError();
         }
@@ -2293,14 +2293,24 @@ public class Parser {
                 }
             }
         } else if (aggregateType == Aggregate.GROUP_CONCAT) {
-            boolean distinct = readIf("DISTINCT");
-            Aggregate agg = new Aggregate(Aggregate.GROUP_CONCAT,
+            Aggregate agg = null;
+            if (equalsToken("GROUP_CONCAT", aggregateName)) {
+                boolean distinct = readIf("DISTINCT");
+                agg = new Aggregate(Aggregate.GROUP_CONCAT,
                     readExpression(), currentSelect, distinct);
-            if (readIf("ORDER")) {
-                read("BY");
-                agg.setGroupConcatOrder(parseSimpleOrderList());
-            }
-            if (readIf("SEPARATOR")) {
+                if (readIf("ORDER")) {
+                    read("BY");
+                    agg.setGroupConcatOrder(parseSimpleOrderList());
+                }
+                
+                if (readIf("SEPARATOR")) {
+                    agg.setGroupConcatSeparator(readExpression());
+                }
+            } else if (equalsToken("STRING_AGG", aggregateName)) {
+                // PostgreSQL compatibility: string_agg(expression, delimiter)
+                agg = new Aggregate(Aggregate.GROUP_CONCAT,
+                    readExpression(), currentSelect, false);
+                read(",");
                 agg.setGroupConcatSeparator(readExpression());
             }
             r = agg;
@@ -2382,7 +2392,7 @@ public class Parser {
         }
         int agg = getAggregateType(name);
         if (agg >= 0) {
-            return readAggregate(agg);
+            return readAggregate(agg, name);
         }
         Function function = Function.getFunction(database, name);
         if (function == null) {

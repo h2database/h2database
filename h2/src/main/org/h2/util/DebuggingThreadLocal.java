a90a803ec2bea3852a40601bd0c40c737a7cef63
Thomas Mueller <mueller@adobe.com>
2015-07-09 07:14:18 +0200

    Formatting

--- h2/src/main/org/h2/util/DebuggingThreadLocal.java
+++ h2/src/main/org/h2/util/DebuggingThreadLocal.java
@@ -9,36 +9,36 @@ import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * Similar to ThreadLocal, except that it allows it's data to be read from other threads - useful for debugging info.
+ * Similar to ThreadLocal, except that it allows it's data to be read from other
+ * threads - useful for debugging info.
+ *
+ * @param <T> the type
  */
-public class DebuggingThreadLocal<T>
-{
-  private final ConcurrentHashMap<Long, T> map = new ConcurrentHashMap<Long, T>();
+public class DebuggingThreadLocal<T> {
 
-	public DebuggingThreadLocal() {
-	}
+    private final ConcurrentHashMap<Long, T> map = new ConcurrentHashMap<Long, T>();
 
-	public void set(T value) {
-		map.put(Thread.currentThread().getId(), value);
-	}
+    public void set(T value) {
+        map.put(Thread.currentThread().getId(), value);
+    }
 
-	public void remove() {
-		map.remove(Thread.currentThread().getId());
-	}
+    public void remove() {
+        map.remove(Thread.currentThread().getId());
+    }
 
-	public T get() {
-		return map.get(Thread.currentThread().getId());
-	}
+    public T get() {
+        return map.get(Thread.currentThread().getId());
+    }
 
-	/**
-	 * @return a HashMap containing a mapping from thread-id to value
-	 */
-	public HashMap<Long,T> getSnapshotOfAllThreads() {
-		return new HashMap<Long,T>(map);
-	}
+    /**
+     * @return a HashMap containing a mapping from thread-id to value
+     */
+    public HashMap<Long, T> getSnapshotOfAllThreads() {
+        return new HashMap<Long, T>(map);
+    }
 
-	public T deepCopy(T value) {
-		return value;
-	}
+    public T deepCopy(T value) {
+        return value;
+    }
 
 }

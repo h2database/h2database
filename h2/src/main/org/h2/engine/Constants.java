7014d46a48638c2b59716fef3262d1681c527b19
Thomas Mueller Graf <thomas.tom.mueller@gmail.com>
2015-09-13 13:12:30 +0200

    Prepare release

--- h2/src/main/org/h2/engine/Constants.java
+++ h2/src/main/org/h2/engine/Constants.java
@@ -16,7 +16,7 @@ public class Constants {
     /**
      * The build date is updated for each public release.
      */
-    public static final String BUILD_DATE = "2015-08-01";
+    public static final String BUILD_DATE = "2015-09-13";
 
     /**
      * The build date of the last stable release.
@@ -26,7 +26,7 @@ public class Constants {
     /**
      * The build id is incremented for each public release.
      */
-    public static final int BUILD_ID = 188;
+    public static final int BUILD_ID = 189;
 
     /**
      * The build id of the last stable release.

fbafea923d531eab69db16d39a89c2fa234857aa
Noel Grandin <noelgrandin@gmail.com>
2015-12-26 14:30:24 +0200

    fix unit test

--- h2/src/test/org/h2/test/unit/TestValueMemory.java
+++ h2/src/test/org/h2/test/unit/TestValueMemory.java
@@ -12,6 +12,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.IdentityHashMap;
 import java.util.Random;
+
 import org.h2.api.JavaObjectSerializer;
 import org.h2.engine.Constants;
 import org.h2.store.DataHandler;
@@ -46,6 +47,7 @@ import org.h2.value.ValueStringFixed;
 import org.h2.value.ValueStringIgnoreCase;
 import org.h2.value.ValueTime;
 import org.h2.value.ValueTimestamp;
+import org.h2.value.ValueTimestampUtc;
 import org.h2.value.ValueUuid;
 
 /**
@@ -162,6 +164,8 @@ public class TestValueMemory extends TestBase implements DataHandler {
             return ValueDate.get(new java.sql.Date(random.nextLong()));
         case Value.TIMESTAMP:
             return ValueTimestamp.fromMillis(random.nextLong());
+        case Value.TIMESTAMP_UTC:
+            return ValueTimestampUtc.fromMillis(random.nextLong());
         case Value.BYTES:
             return ValueBytes.get(randomBytes(random.nextInt(1000)));
         case Value.STRING:

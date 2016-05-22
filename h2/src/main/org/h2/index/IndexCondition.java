6e6284aca861dafbafe6428b99c96691b6204ab1
lukaseder <lukas.eder@gmail.com>
2016-05-22 12:50:28 +0200

    Revert unrelated change

--- h2/src/main/org/h2/index/IndexCondition.java
+++ h2/src/main/org/h2/index/IndexCondition.java
@@ -401,7 +401,7 @@ public class IndexCondition {
         return "column=" + column +
                 ", compareType=" + compareTypeToString(compareType) +
                 ", expression=" + expression +
-                ", expressionList=" + expressionList +
+                ", expressionList=" + expressionList.toString() +
                 ", expressionQuery=" + expressionQuery;
     }
 

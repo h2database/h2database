ca4e29e73eaa280c7f1cc531701a0356288835a3
nicolas-f <github@nettrader.fr>
2016-06-09 09:45:30 +0200

    fix unit test variable name

--- h2/src/test/org/h2/test/db/TestSpatial.java
+++ h2/src/test/org/h2/test/db/TestSpatial.java
@@ -1081,7 +1081,7 @@ public class TestSpatial extends TestBase {
 
     public void testIndexUpdateNullGeometry() throws SQLException {
         deleteDb("spatial");
-        Connection conn = getConnection(url);
+        Connection conn = getConnection(URL);
         Statement stat = conn.createStatement();
         stat.execute("drop table if exists DUMMY_11;");
         stat.execute("CREATE TABLE PUBLIC.DUMMY_11 (fid serial,  GEOM GEOMETRY);");
@@ -1119,7 +1119,7 @@ public class TestSpatial extends TestBase {
 
     public void testInsertNull() throws SQLException {
         deleteDb("spatial");
-        Connection conn = getConnection(url);
+        Connection conn = getConnection(URL);
         Statement stat = conn.createStatement();
         stat.execute("\n" +
                 "drop table if exists PUBLIC.DUMMY_12;\n" +

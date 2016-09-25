adfa0261d5cea5d9fb0d2b32bd89b1e76786f958
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2016-09-25 03:44:28 +0300

    fix compare TIMESTAMP WITH TIMEZONE

--- h2/src/test/org/h2/test/unit/TestTimeStampWithTimeZone.java
+++ h2/src/test/org/h2/test/unit/TestTimeStampWithTimeZone.java
@@ -11,6 +11,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import org.h2.api.TimestampWithTimeZone;
 import org.h2.test.TestBase;
+import org.h2.value.ValueTimestampTimeZone;
 
 /**
  */
@@ -29,6 +30,10 @@ public class TestTimeStampWithTimeZone extends TestBase {
     public void test() throws SQLException {
         deleteDb(getTestName());
         test1();
+        test2();
+        test3();
+        test4();
+        testOrder();
         deleteDb(getTestName());
     }
 
@@ -78,6 +83,39 @@ public class TestTimeStampWithTimeZone extends TestBase {
         assertEquals(31, ts.getDay());
         rs.close();
         stat.close();
+        conn.close();
+    }
+
+    private void test2() {
+    	ValueTimestampTimeZone a = ValueTimestampTimeZone.parse("1970-01-01 12:00:00.00+00:15");
+    	ValueTimestampTimeZone b = ValueTimestampTimeZone.parse("1970-01-01 12:00:01.00+01:15");
+    	int c= a.compareTo(b, null);
+    	assertEquals(c, 1);
+    }
+    
+    private void test3() {
+    	ValueTimestampTimeZone a = ValueTimestampTimeZone.parse("1970-01-02 00:00:02.00+01:15");
+    	ValueTimestampTimeZone b = ValueTimestampTimeZone.parse("1970-01-01 23:00:01.00+00:15");
+    	int c= a.compareTo(b, null);
+    	assertEquals(c, 1);
+    }
+
+    private void test4() {
+    	ValueTimestampTimeZone a = ValueTimestampTimeZone.parse("1970-01-02 00:00:01.00+01:15");
+    	ValueTimestampTimeZone b = ValueTimestampTimeZone.parse("1970-01-01 23:00:01.00+00:15");
+    	int c= a.compareTo(b, null);
+    	assertEquals(c, 0);
+    }
+
+    private void testOrder() throws SQLException {
+        Connection conn = getConnection(getTestName());
+        Statement stat = conn.createStatement();
+        stat.execute("create table test_order(id identity, t1 timestamp with timezone)");
+        stat.execute("insert into test_order(t1) values('1970-01-01 12:00:00.00+00:15')");
+        stat.execute("insert into test_order(t1) values('1970-01-01 12:00:01.00+01:15')");
+        ResultSet rs = stat.executeQuery("select t1 from test_order order by t1");
+        rs.next();
+        assertEquals("1970-01-01 12:00:01.0+01:15", rs.getString(1));
         conn.close();
     }
 

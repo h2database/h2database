7cc659a5deaf4ff078314c2092504622183ebd7e
Philippe Marschall <philippe.marschall@gmail.com>
2016-10-05 19:35:06 +0200

    Remove Java 6 backwards compatibility
    Since we now require at least Java 6 we can remove the Java 6 backwards
    compatibility layer.

--- h2/src/main/org/h2/jdbcx/JdbcConnectionPoolBackwardsCompat.java
+++ h2/src/main/org/h2/jdbcx/JdbcConnectionPoolBackwardsCompat.java
@@ -5,12 +5,9 @@
  */
 package org.h2.jdbcx;
 
-import java.util.logging.Logger;
-
 /**
  * Allows us to compile on older platforms, while still implementing the methods from the newer JDBC API.
  */
 public interface JdbcConnectionPoolBackwardsCompat {
 
-    Logger getParentLogger();
 }

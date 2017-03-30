8665d5e0464877df833d0757154e682b0bbfb393
Noel Grandin <noelgrandin@gmail.com>
2017-03-30 12:01:21 +0200

    add missing license headers

--- h2/src/main/org/h2/value/ValueEnum.java
+++ h2/src/main/org/h2/value/ValueEnum.java
@@ -1,11 +1,14 @@
+/*
+ * Copyright 2004-2014 H2 Group. Multiple-Licensed under the MPL 2.0,
+ * and the EPL 1.0 (http://h2database.com/html/license.html).
+ * Initial Developer: H2 Group
+ */
 package org.h2.value;
 
 import java.util.Locale;
-
 import org.h2.api.ErrorCode;
 import org.h2.message.DbException;
 import org.h2.util.MathUtils;
-import org.h2.value.DataType;
 
 public class ValueEnum extends ValueEnumBase {
     private static enum Validation {
--- h2/src/main/org/h2/value/ValueEnumBase.java
+++ h2/src/main/org/h2/value/ValueEnumBase.java
@@ -1,10 +1,12 @@
+/*
+ * Copyright 2004-2014 H2 Group. Multiple-Licensed under the MPL 2.0,
+ * and the EPL 1.0 (http://h2database.com/html/license.html).
+ * Initial Developer: H2 Group
+ */
 package org.h2.value;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Locale;
-
-import org.h2.message.DbException;
 import org.h2.util.MathUtils;
 
 /**

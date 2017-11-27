efd4de99384610633a932edfcec841ac97990f08
Noel Grandin <noelgrandin@gmail.com>
2017-11-27 15:51:58 +0200

    Issue #650: Simple nested SELECT causes error for table with TIMESTAMP WITH TIMEZONE column

--- h2/src/test/org/h2/test/scripts/datatypes/timestamp-with-timezone.sql
+++ h2/src/test/org/h2/test/scripts/datatypes/timestamp-with-timezone.sql
@@ -2,3 +2,15 @@
 -- and the EPL 1.0 (http://h2database.com/html/license.html).
 -- Initial Developer: H2 Group
 --
+
+
+CREATE TABLE tab_with_timezone(x TIMESTAMP WITH TIME ZONE);
+> ok
+
+INSERT INTO tab_with_timezone(x) VALUES ('2017-01-01');
+> update count: 1
+
+SELECT "Query".* FROM (select * from tab_with_timezone where x > '2016-01-01') AS "Query";
+> X
+> ------------------------
+> 2017-01-01 00:00:00.0+00

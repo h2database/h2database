81aee733b0724b9f2676a350579d22abb7baa9f3
Evgenij Ryazanov <katzyn@gmail.com>
2018-01-09 16:35:09 +0800

    Fix tests of degrees() and radians() for Java 9

--- h2/src/test/org/h2/test/scripts/functions/numeric/degrees.sql
+++ h2/src/test/org/h2/test/scripts/functions/numeric/degrees.sql
@@ -9,11 +9,12 @@ create memory table test(id int primary key, name varchar(255));
 insert into test values(1, 'Hello');
 > update count: 1
 
-select degrees(null) vn, degrees(1) v1, degrees(1.1) v2, degrees(-1.1) v3, degrees(1.9) v4, degrees(-1.9) v5 from test;
-> VN   V1                V2                V3                 V4                 V5
-> ---- ----------------- ----------------- ------------------ ------------------ -------------------
-> null 57.29577951308232 63.02535746439057 -63.02535746439057 108.86198107485642 -108.86198107485642
+-- Truncate least significant digits because implementations returns slightly
+-- different results depending on Java version
+select degrees(null) vn, truncate(degrees(1), 10) v1, truncate(degrees(1.1), 10) v2,
+    truncate(degrees(-1.1), 10) v3, truncate(degrees(1.9), 10) v4,
+    truncate(degrees(-1.9), 10) v5 from test;
+> VN   V1           V2            V3             V4             V5
+> ---- ------------ ------------- -------------- -------------- ---------------
+> null 57.295779513 63.0253574643 -63.0253574643 108.8619810748 -108.8619810748
 > rows: 1
-
-
-

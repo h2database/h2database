d90ddbf6977a3fc659d520957867fd957660f549
Evgenij Ryazanov <katzyn@gmail.com>
2018-01-11 11:09:51 +0800

    Do not initialize private constant arrays in method

--- h2/src/main/org/h2/util/ToChar.java
+++ h2/src/main/org/h2/util/ToChar.java
@@ -31,6 +31,12 @@ public class ToChar {
      */
     private static final long JULIAN_EPOCH;
 
+    private static final int[] ROMAN_VALUES = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9,
+            5, 4, 1 };
+
+    private static final String[] ROMAN_NUMERALS = { "M", "CM", "D", "CD", "C", "XC",
+            "L", "XL", "X", "IX", "V", "IV", "I" };
+
     static {
         GregorianCalendar epoch = new GregorianCalendar(Locale.ENGLISH);
         epoch.setGregorianChange(new Date(Long.MAX_VALUE));
@@ -410,14 +416,10 @@ public class ToChar {
     }
 
     private static String toRomanNumeral(int number) {
-        int[] values = new int[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9,
-                5, 4, 1 };
-        String[] numerals = new String[] { "M", "CM", "D", "CD", "C", "XC",
-                "L", "XL", "X", "IX", "V", "IV", "I" };
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < values.length; i++) {
-            int value = values[i];
-            String numeral = numerals[i];
+        for (int i = 0; i < ROMAN_VALUES.length; i++) {
+            int value = ROMAN_VALUES[i];
+            String numeral = ROMAN_NUMERALS[i];
             while (number >= value) {
                 result.append(numeral);
                 number -= value;

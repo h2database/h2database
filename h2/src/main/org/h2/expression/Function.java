30246a7406551d3ea1d346bbd9a08bd4a4911d68
Evgenij Ryazanov <katzyn@gmail.com>
2018-03-06 15:15:23 +0800

    Use Math.log10() for LOG10

--- h2/src/main/org/h2/expression/Function.java
+++ h2/src/main/org/h2/expression/Function.java
@@ -620,10 +620,6 @@ public class Function extends Expression implements FunctionCall {
         }
     }
 
-    private static strictfp double log10(double value) {
-        return roundMagic(StrictMath.log(value) / StrictMath.log(10));
-    }
-
     @Override
     public Value getValue(Session session) {
         return getValueWithArgs(session, args);
@@ -682,7 +678,7 @@ public class Function extends Expression implements FunctionCall {
             }
             break;
         case LOG10:
-            result = ValueDouble.get(log10(v0.getDouble()));
+            result = ValueDouble.get(Math.log10(v0.getDouble()));
             break;
         case PI:
             result = ValueDouble.get(Math.PI);

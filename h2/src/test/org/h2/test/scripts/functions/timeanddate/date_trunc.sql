d5f3df0d387c0550cb2bbd67f981139244b5e930
Stéphane Eintrazi <7600006+ScaY@users.noreply.github.com>
2018-03-08 23:04:20 +0100

    add test with day in uppercase

--- h2/src/test/org/h2/test/scripts/functions/timeanddate/date_trunc.sql
+++ h2/src/test/org/h2/test/scripts/functions/timeanddate/date_trunc.sql
@@ -1,23 +1,47 @@
 select DATE_TRUNC('day', time '00:00:00');
 >> 00:00:00
 
+select DATE_TRUNC('DAY', time '00:00:00');
+>> 00:00:00
+
 select DATE_TRUNC('day', time '15:14:13');
+>> 00:00:00
+
+select DATE_TRUNC('DAY', time '15:14:13');
 >> 00:00:00
 
 select DATE_TRUNC('day', date '2015-05-29');
 >> 2015-05-29
 
+select DATE_TRUNC('DAY', date '2015-05-29');
+>> 2015-05-29
+
 select DATE_TRUNC('day', timestamp '2015-05-29 15:14:13');
+>> 2015-05-29 00:00:00
+
+select DATE_TRUNC('DAY', timestamp '2015-05-29 15:14:13');
 >> 2015-05-29 00:00:00
 
 select DATE_TRUNC('day', timestamp with time zone '2015-05-29 15:14:13');
 >> 2015-05-29 00:00:00+00
 
+select DATE_TRUNC('DAY', timestamp with time zone '2015-05-29 15:14:13');
+>> 2015-05-29 00:00:00+00
+
 select DATE_TRUNC('day', timestamp with time zone '2015-05-29 05:14:13-06');
+>> 2015-05-29 00:00:00-06
+
+select DATE_TRUNC('DAY', timestamp with time zone '2015-05-29 05:14:13-06');
 >> 2015-05-29 00:00:00-06
 
 select DATE_TRUNC('day', timestamp with time zone '2015-05-29 15:14:13+10');
 >> 2015-05-29 00:00:00+10
 
+select DATE_TRUNC('DAY', timestamp with time zone '2015-05-29 15:14:13+10');
+>> 2015-05-29 00:00:00+10
+
 select DATE_TRUNC('day', '2015-05-29 15:14:13');
+>> 2015-05-29 00:00:00
+
+select DATE_TRUNC('DAY', '2015-05-29 15:14:13');
 >> 2015-05-29 00:00:00

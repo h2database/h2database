4445ec63cb211859dfd27c9d0175e32af0172489
Stéphane Eintrazi <stephane.viper@gmail.com>
2018-03-09 21:28:16 +0100

    add test on other possible values

--- h2/src/test/org/h2/test/scripts/functions/timeanddate/date_trunc.sql
+++ h2/src/test/org/h2/test/scripts/functions/timeanddate/date_trunc.sql
@@ -45,3 +45,78 @@ select DATE_TRUNC('day', '2015-05-29 15:14:13');
 
 select DATE_TRUNC('DAY', '2015-05-29 15:14:13');
 >> 2015-05-29 00:00:00
+
+SELECT DATE_TRUNC('---', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('microseconds', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('MICROSECONDS', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('milliseconds', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('MILLISECONDS', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('second', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('SECOND', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('minute', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('MINUTE', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('hour', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('HOUR', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('week', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('WEEK', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('month', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('MONTH', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('quarter', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('QUARTER', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('year', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('YEAR', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('decade', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('DECADE', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('century', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('CENTURY', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('millennium', '2015-05-29 15:14:13');
+> exception
+
+SELECT DATE_TRUNC('MILLENNIUM', '2015-05-29 15:14:13');
+> exception

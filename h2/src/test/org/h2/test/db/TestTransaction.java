7034de301f3ff8435fbfd7ebbfd7912ba9be4480
Evgenij Ryazanov <katzyn@gmail.com>
2018-04-28 21:59:18 +0800

    Remove debug change

--- h2/src/test/org/h2/test/db/TestTransaction.java
+++ h2/src/test/org/h2/test/db/TestTransaction.java
@@ -35,7 +35,6 @@ public class TestTransaction extends TestBase {
 
     @Override
     public void test() throws SQLException {
-        config.mvStore = false;
         testClosingConnectionWithSessionTempTable();
         testClosingConnectionWithLockedTable();
         testConstraintCreationRollback();

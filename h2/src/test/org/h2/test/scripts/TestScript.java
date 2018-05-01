ad96fd7cd75df5a91fa5d297367f68b22a453df8
Evgenij Ryazanov <katzyn@gmail.com>
2018-05-01 17:09:17 +0800

    Extract comments tests from testScript.sql to comments.sql

--- h2/src/test/org/h2/test/scripts/TestScript.java
+++ h2/src/test/org/h2/test/scripts/TestScript.java
@@ -86,6 +86,7 @@ public class TestScript extends TestBase {
         reconnectOften = !config.memory && config.big;
 
         testScript("testScript.sql");
+        testScript("comments.sql");
         testScript("derived-column-names.sql");
         testScript("dual.sql");
         testScript("indexes.sql");

a8748cb12962794ed519017a66148aa97bed6786
Evgenij Ryazanov <katzyn@gmail.com>
2018-05-13 12:44:01 +0800

    Read fields directly in Schema.getAll()

--- h2/src/main/org/h2/schema/Schema.java
+++ h2/src/main/org/h2/schema/Schema.java
@@ -595,14 +595,14 @@ public class Schema extends DbObjectBase {
      */
     public ArrayList<SchemaObject> getAll() {
         ArrayList<SchemaObject> all = Utils.newSmallArrayList();
-        all.addAll(getMap(DbObject.TABLE_OR_VIEW).values());
-        all.addAll(getMap(DbObject.SYNONYM).values());
-        all.addAll(getMap(DbObject.SEQUENCE).values());
-        all.addAll(getMap(DbObject.INDEX).values());
-        all.addAll(getMap(DbObject.TRIGGER).values());
-        all.addAll(getMap(DbObject.CONSTRAINT).values());
-        all.addAll(getMap(DbObject.CONSTANT).values());
-        all.addAll(getMap(DbObject.FUNCTION_ALIAS).values());
+        all.addAll(tablesAndViews.values());
+        all.addAll(synonyms.values());
+        all.addAll(sequences.values());
+        all.addAll(indexes.values());
+        all.addAll(triggers.values());
+        all.addAll(constraints.values());
+        all.addAll(constants.values());
+        all.addAll(functions.values());
         return all;
     }
 

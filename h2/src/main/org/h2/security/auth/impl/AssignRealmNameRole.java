74de799212a8ca3ea9fef241270b31c07ee237c5
Evgenij Ryazanov <katzyn@gmail.com>
2018-06-17 16:35:44 +0800

    Add missing package.html and copyright headers

--- h2/src/main/org/h2/security/auth/impl/AssignRealmNameRole.java
+++ h2/src/main/org/h2/security/auth/impl/AssignRealmNameRole.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2004-2018 H2 Group. Multiple-Licensed under the MPL 2.0,
+ * and the EPL 1.0 (http://h2database.com/html/license.html).
+ * Initial Developer: Alessandro Ventura
+ */
 package org.h2.security.auth.impl;
 
 import java.util.Arrays;

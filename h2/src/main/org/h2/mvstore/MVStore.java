9bcb29425cec74625b49ac331a4ff49df52450cc
Andrei Tokar <andrei.tokar@gmail.com>
2018-09-21 17:58:25 -0400

    release lock even if on compaction failure

--- h2/src/main/org/h2/mvstore/MVStore.java
+++ h2/src/main/org/h2/mvstore/MVStore.java
@@ -943,24 +943,27 @@ public class MVStore {
         closed = true;
         storeLock.lock();
         try {
-            if (fileStore != null && shrinkIfPossible) {
-                shrinkFileIfPossible(0);
-            }
-            // release memory early - this is important when called
-            // because of out of memory
-            if (cache != null) {
-                cache.clear();
-            }
-            if (cacheChunkRef != null) {
-                cacheChunkRef.clear();
-            }
-            for (MVMap<?, ?> m : new ArrayList<>(maps.values())) {
-                m.close();
-            }
-            chunks.clear();
-            maps.clear();
-            if (fileStore != null && !fileStoreIsProvided) {
-                fileStore.close();
+            try {
+                if (fileStore != null && shrinkIfPossible) {
+                    shrinkFileIfPossible(0);
+                }
+                // release memory early - this is important when called
+                // because of out of memory
+                if (cache != null) {
+                    cache.clear();
+                }
+                if (cacheChunkRef != null) {
+                    cacheChunkRef.clear();
+                }
+                for (MVMap<?, ?> m : new ArrayList<>(maps.values())) {
+                    m.close();
+                }
+                chunks.clear();
+                maps.clear();
+            } finally {
+                if (fileStore != null && !fileStoreIsProvided) {
+                    fileStore.close();
+                }
             }
         } finally {
             storeLock.unlock();

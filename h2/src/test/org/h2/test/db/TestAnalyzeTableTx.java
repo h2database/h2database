019773f78ad3116a93c1e685ec214918377e7e05
Andrei Tokar <andrei.tokar@gmail.com>
2018-12-24 21:58:15 -0500

    scale down test to fit into memory

--- h2/src/test/org/h2/test/db/TestAnalyzeTableTx.java
+++ h2/src/test/org/h2/test/db/TestAnalyzeTableTx.java
@@ -12,7 +12,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class TestAnalyzeTableTx extends TestDb {
-    static final int C = 100_000;
+    static final int C = 25_000;
 
     /**
      * Run just this test.

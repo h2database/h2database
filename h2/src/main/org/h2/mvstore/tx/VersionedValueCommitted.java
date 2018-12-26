d16fcd9003659e2147a33976a2436ae9dd96fcf0
Andrei Tokar <andrei.tokar@gmail.com>
2018-12-26 05:51:01 -0500

    copyright

--- h2/src/main/org/h2/mvstore/tx/VersionedValueCommitted.java
+++ h2/src/main/org/h2/mvstore/tx/VersionedValueCommitted.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2017 H2 Group. Multiple-Licensed under the MPL 2.0,
+ * Copyright 2004-2018 H2 Group. Multiple-Licensed under the MPL 2.0,
  * and the EPL 1.0 (http://h2database.com/html/license.html).
  * Initial Developer: H2 Group
  */
@@ -9,9 +9,6 @@ import org.h2.value.VersionedValue;
 
 /**
  * Class CommittedVersionedValue.
- * <UL>
- * <LI> 12/24/18 8:48 PM initial creation
- * </UL>
  *
  * @author <a href='mailto:andrei.tokar@gmail.com'>Andrei Tokar</a>
  */
--- h2/src/main/org/h2/mvstore/tx/VersionedValueUncommitted.java
+++ h2/src/main/org/h2/mvstore/tx/VersionedValueUncommitted.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2017 H2 Group. Multiple-Licensed under the MPL 2.0,
+ * Copyright 2004-2018 H2 Group. Multiple-Licensed under the MPL 2.0,
  * and the EPL 1.0 (http://h2database.com/html/license.html).
  * Initial Developer: H2 Group
  */
@@ -9,9 +9,6 @@ import org.h2.value.VersionedValue;
 
 /**
  * Class VersionedValueUncommitted.
- * <UL>
- * <LI> 12/24/18 8:49 PM initial creation
- * </UL>
  *
  * @author <a href='mailto:andrei.tokar@gmail.com'>Andrei Tokar</a>
  */
@@ -19,7 +16,7 @@ class VersionedValueUncommitted extends VersionedValueCommitted {
     private final long operationId;
     private final Object committedValue;
 
-    VersionedValueUncommitted(long operationId, Object value, Object committedValue) {
+    private VersionedValueUncommitted(long operationId, Object value, Object committedValue) {
         super(value);
         assert operationId != 0;
         this.operationId = operationId;

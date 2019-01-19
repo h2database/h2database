2cdafd3a6084cf9c8a1506b7dc3d273c464e9333
Evgenij Ryazanov <katzyn@gmail.com>
2019-01-19 15:29:52 +0800

    Use TreeMap in MergeUsing for safety

--- h2/src/main/org/h2/command/dml/MergeUsing.java
+++ h2/src/main/org/h2/command/dml/MergeUsing.java
@@ -6,8 +6,8 @@
 package org.h2.command.dml;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.TreeMap;
 
 import org.h2.api.ErrorCode;
 import org.h2.api.Trigger;
@@ -278,7 +278,7 @@ public class MergeUsing extends Prepared {
     private String queryAlias;
     private int countUpdatedRows;
     private Select targetMatchQuery;
-    private final HashMap<Value, Integer> targetRowidsRemembered = new HashMap<>();
+    private final TreeMap<Value, Integer> targetRowidsRemembered;
     private int sourceQueryRowNumber;
 
 
@@ -286,6 +286,7 @@ public class MergeUsing extends Prepared {
         super(session);
         this.targetTable = targetTableFilter.getTable();
         this.targetTableFilter = targetTableFilter;
+        targetRowidsRemembered = new TreeMap<>(session.getDatabase().getCompareMode());
     }
 
     @Override

260e8f8473fc4f5c85a3b500b80646c2a89b2920
Lazarev Nikita <lazarevn@ispras.ru>
2019-02-20 15:43:18 +0300

    JSON type on transfer layer

--- h2/src/main/org/h2/value/Transfer.java
+++ h2/src/main/org/h2/value/Transfer.java
@@ -69,6 +69,7 @@ public class Transfer {
     private static final int ENUM = 25;
     private static final int INTERVAL = 26;
     private static final int ROW = 27;
+    private static final int JSON = 28;
 
     private Socket socket;
     private DataInputStream in;
@@ -606,6 +607,9 @@ public class Transfer {
                 writeString(v.getString());
             }
             break;
+        case Value.JSON:
+            writeInt(JSON);
+            writeString(v.getString());
         default:
             if (JdbcUtils.customDataTypesHandler != null) {
                 writeInt(type);
@@ -777,6 +781,10 @@ public class Transfer {
             }
             return ValueInterval.from(IntervalQualifier.valueOf(ordinal), negative, readLong(),
                     ordinal < 5 ? 0 : readLong());
+        }
+        case JSON: {
+            String s = readString();
+            return ValueJson.get(s);
         }
         default:
             if (JdbcUtils.customDataTypesHandler != null) {

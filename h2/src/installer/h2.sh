1460cb7784a0c8204b78cbd32adbca5d8cf63ea0
Lazarev Nikita <lazarevn@ispras.ru>
2019-02-20 19:15:46 +0300

    Adding jackson library to classpath

--- h2/src/installer/h2.sh
+++ h2/src/installer/h2.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 dir=$(dirname "$0")
+ext_dir="$dir/../ext"
+CLASSPATH="$CLASSPATH:$ext_dir/jackson-core-2.9.8.jar:$ext_dir/jackson-databind-2.9.8.jar:$ext_dir/jackson-annotations-2.9.8.jar"
+export CLASSPATH
+echo $CLASSPATH
 java -cp "$dir/h2.jar:$H2DRIVERS:$CLASSPATH" org.h2.tools.Console "$@"

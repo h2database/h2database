af8005feebb2076449e995fb0c6f428211f415b8
Lazarev Nikita <lazarevn@ispras.ru>
2019-03-22 18:33:44 +0300

    copyright in ValueJson

--- h2/src/main/org/h2/value/ValueJson.java
+++ h2/src/main/org/h2/value/ValueJson.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2004-2019 H2 Group. Multiple-Licensed under the MPL 2.0,
+ * and the EPL 1.0 (http://h2database.com/html/license.html).
+ * Initial Developer: Lazarev Nikita <lazarevn@ispras.ru>
+ */
 package org.h2.value;
 
 import java.sql.PreparedStatement;

d8be1da3290b9f5a75dd43a2676f2aba87bac2c5
Evgenij Ryazanov <katzyn@gmail.com>
2019-05-11 09:39:57 +0800

    Do not fail DROP TABLE ... RESTRICT when dependency is going to be removed by the same command

--- h2/src/main/org/h2/command/ddl/DropTable.java
+++ h2/src/main/org/h2/command/ddl/DropTable.java
@@ -6,6 +6,7 @@
 package org.h2.command.ddl;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import org.h2.api.ErrorCode;
@@ -20,119 +21,121 @@ import org.h2.schema.Schema;
 import org.h2.table.Table;
 import org.h2.table.TableView;
 import org.h2.util.StringUtils;
+import org.h2.util.Utils;
 
 /**
  * This class represents the statement
  * DROP TABLE
  */
-public class DropTable extends SchemaCommand {
+public class DropTable extends DefineCommand {
+
+    private static final class SchemaAndTable {
+
+        final Schema schema;
+
+        final String tableName;
+
+        SchemaAndTable(Schema schema, String tableName) {
+            this.schema = schema;
+            this.tableName = tableName;
+        }
+
+    }
 
     private boolean ifExists;
-    private String tableName;
-    private Table table;
-    private DropTable next;
     private ConstraintActionType dropAction;
 
-    public DropTable(Session session, Schema schema) {
-        super(session, schema);
+    private final ArrayList<SchemaAndTable> tables = Utils.newSmallArrayList();
+
+    public DropTable(Session session) {
+        super(session);
         dropAction = session.getDatabase().getSettings().dropRestrict ?
                 ConstraintActionType.RESTRICT :
                     ConstraintActionType.CASCADE;
     }
 
-    /**
-     * Chain another drop table statement to this statement.
-     *
-     * @param drop the statement to add
-     */
-    public void addNextDropTable(DropTable drop) {
-        if (next == null) {
-            next = drop;
-        } else {
-            next.addNextDropTable(drop);
-        }
-    }
-
     public void setIfExists(boolean b) {
         ifExists = b;
-        if (next != null) {
-            next.setIfExists(b);
+    }
+
+    public void addTable(Schema schema, String tableName) {
+        tables.add(new SchemaAndTable(schema, tableName));
+    }
+
+    private boolean prepareDrop() {
+        HashSet<Table> tablesToDrop = new HashSet<>();
+        for (SchemaAndTable schemaAndTable : tables) {
+            String tableName = schemaAndTable.tableName;
+            Table table = schemaAndTable.schema.findTableOrView(session, tableName);
+            if (table == null) {
+                if (!ifExists) {
+                    throw DbException.get(ErrorCode.TABLE_OR_VIEW_NOT_FOUND_1, tableName);
+                }
+            } else {
+                session.getUser().checkRight(table, Right.ALL);
+                if (!table.canDrop()) {
+                    throw DbException.get(ErrorCode.CANNOT_DROP_TABLE_1, tableName);
+                }
+                tablesToDrop.add(table);
+            }
         }
-    }
-
-    public void setTableName(String tableName) {
-        this.tableName = tableName;
-    }
-
-    private void prepareDrop() {
-        table = getSchema().findTableOrView(session, tableName);
-        if (table == null) {
-            if (!ifExists) {
-                throw DbException.get(ErrorCode.TABLE_OR_VIEW_NOT_FOUND_1, tableName);
-            }
-        } else {
-            session.getUser().checkRight(table, Right.ALL);
-            if (!table.canDrop()) {
-                throw DbException.get(ErrorCode.CANNOT_DROP_TABLE_1, tableName);
-            }
+        if (tablesToDrop.isEmpty()) {
+            return false;
+        }
+        for (Table table : tablesToDrop) {
+            ArrayList<String> dependencies = new ArrayList<>();
             if (dropAction == ConstraintActionType.RESTRICT) {
-                ArrayList<String> dependencies = new ArrayList<>();
                 CopyOnWriteArrayList<TableView> dependentViews = table.getDependentViews();
                 if (dependentViews != null && !dependentViews.isEmpty()) {
                     for (TableView v : dependentViews) {
-                        dependencies.add(v.getName());
+                        if (!tablesToDrop.contains(v)) {
+                            dependencies.add(v.getName());
+                        }
                     }
                 }
                 final List<Constraint> constraints = table.getConstraints();
                 if (constraints != null && !constraints.isEmpty()) {
                     for (Constraint c : constraints) {
-                        if (c.getTable() != table) {
+                        if (!tablesToDrop.contains(c.getTable())) {
                             dependencies.add(c.getName());
                         }
                     }
                 }
                 if (!dependencies.isEmpty()) {
-                    throw DbException.get(ErrorCode.CANNOT_DROP_2, tableName,
+                    throw DbException.get(ErrorCode.CANNOT_DROP_2, table.getName(),
                             StringUtils.join(new StringBuilder(), dependencies, ", ").toString());
                 }
-
             }
             table.lock(session, true, true);
         }
-        if (next != null) {
-            next.prepareDrop();
-        }
+        return true;
     }
 
     private void executeDrop() {
-        // need to get the table again, because it may be dropped already
-        // meanwhile (dependent object, or same object)
-        table = getSchema().findTableOrView(session, tableName);
-
-        if (table != null) {
-            table.setModified();
-            Database db = session.getDatabase();
-            db.lockMeta(session);
-            db.removeSchemaObject(session, table);
-        }
-        if (next != null) {
-            next.executeDrop();
+        for (SchemaAndTable schemaAndTable : tables) {
+            // need to get the table again, because it may be dropped already
+            // meanwhile (dependent object, or same object)
+            Table table = schemaAndTable.schema.findTableOrView(session, schemaAndTable.tableName);
+            if (table != null) {
+                table.setModified();
+                Database db = session.getDatabase();
+                db.lockMeta(session);
+                db.removeSchemaObject(session, table);
+            }
         }
     }
 
     @Override
     public int update() {
         session.commit(true);
-        prepareDrop();
-        executeDrop();
+        if (prepareDrop()) {
+            executeDrop();
+        }
         return 0;
     }
 
     public void setDropAction(ConstraintActionType dropAction) {
         this.dropAction = dropAction;
-        if (next != null) {
-            next.setDropAction(dropAction);
-        }
     }
 
     @Override

6292585688d5d3f7cf3a74e3bb3d3a184b6e1a32
Evgenij Ryazanov <katzyn@gmail.com>
2019-05-14 13:58:31 +0800

    Move TRANSLATE() tests to translate.sql

--- h2/src/test/org/h2/test/scripts/functions/string/translate.sql
+++ h2/src/test/org/h2/test/scripts/functions/string/translate.sql
@@ -2,3 +2,24 @@
 -- and the EPL 1.0 (https://h2database.com/html/license.html).
 -- Initial Developer: H2 Group
 --
+
+CREATE TABLE testTranslate(id BIGINT, txt1 VARCHAR);
+> ok
+
+INSERT INTO testTranslate(id, txt1) values(1, 'test1'), (2, NULL), (3, ''), (4, 'caps');
+> update count: 4
+
+SELECT TRANSLATE(txt1, 'p', 'r') FROM testTranslate ORDER BY id;
+> TRANSLATE(TXT1, 'p', 'r')
+> -------------------------
+> test1
+> null
+>
+> cars
+> rows (ordered): 4
+
+SELECT TRANSLATE(NULL, NULL, NULL);
+>> null
+
+DROP TABLE testTranslate;
+> ok

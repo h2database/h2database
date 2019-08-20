621ba1826f0f3ba695b87f38e5500b286bc40c70
Evgenij Ryazanov <katzyn@gmail.com>
2019-08-20 13:09:50 +0800

    Do not pass queries to DatabaseMetaData.getColumns()

--- h2/src/main/org/h2/table/TableLink.java
+++ h2/src/main/org/h2/table/TableLink.java
@@ -120,48 +120,52 @@ public class TableLink extends Table {
         storesMixedCase = meta.storesMixedCaseIdentifiers();
         storesMixedCaseQuoted = meta.storesMixedCaseQuotedIdentifiers();
         supportsMixedCaseIdentifiers = meta.supportsMixedCaseIdentifiers();
-        ResultSet rs = meta.getTables(null, originalSchema, originalTable, null);
-        if (rs.next() && rs.next()) {
-            throw DbException.get(ErrorCode.SCHEMA_NAME_MUST_MATCH, originalTable);
-        }
-        rs.close();
-        rs = meta.getColumns(null, originalSchema, originalTable, null);
-        int i = 0;
         ArrayList<Column> columnList = Utils.newSmallArrayList();
         HashMap<String, Column> columnMap = new HashMap<>();
-        String catalog = null, schema = null;
-        while (rs.next()) {
-            String thisCatalog = rs.getString("TABLE_CAT");
-            if (catalog == null) {
-                catalog = thisCatalog;
+        String schema = null;
+        boolean isQuery = originalTable.startsWith("(");
+        if (!isQuery) {
+            ResultSet rs = meta.getTables(null, originalSchema, originalTable, null);
+            if (rs.next() && rs.next()) {
+                throw DbException.get(ErrorCode.SCHEMA_NAME_MUST_MATCH, originalTable);
             }
-            String thisSchema = rs.getString("TABLE_SCHEM");
-            if (schema == null) {
-                schema = thisSchema;
+            rs.close();
+            rs = meta.getColumns(null, originalSchema, originalTable, null);
+            int i = 0;
+            String catalog = null;
+            while (rs.next()) {
+                String thisCatalog = rs.getString("TABLE_CAT");
+                if (catalog == null) {
+                    catalog = thisCatalog;
+                }
+                String thisSchema = rs.getString("TABLE_SCHEM");
+                if (schema == null) {
+                    schema = thisSchema;
+                }
+                if (!Objects.equals(catalog, thisCatalog) ||
+                        !Objects.equals(schema, thisSchema)) {
+                    // if the table exists in multiple schemas or tables,
+                    // use the alternative solution
+                    columnMap.clear();
+                    columnList.clear();
+                    break;
+                }
+                String n = rs.getString("COLUMN_NAME");
+                n = convertColumnName(n);
+                int sqlType = rs.getInt("DATA_TYPE");
+                String sqlTypeName = rs.getString("TYPE_NAME");
+                long precision = rs.getInt("COLUMN_SIZE");
+                precision = convertPrecision(sqlType, precision);
+                int scale = rs.getInt("DECIMAL_DIGITS");
+                scale = convertScale(sqlType, scale);
+                int type = DataType.convertSQLTypeToValueType(sqlType, sqlTypeName);
+                Column col = new Column(n, TypeInfo.getTypeInfo(type, precision, scale, null));
+                col.setTable(this, i++);
+                columnList.add(col);
+                columnMap.put(n, col);
             }
-            if (!Objects.equals(catalog, thisCatalog) ||
-                    !Objects.equals(schema, thisSchema)) {
-                // if the table exists in multiple schemas or tables,
-                // use the alternative solution
-                columnMap.clear();
-                columnList.clear();
-                break;
-            }
-            String n = rs.getString("COLUMN_NAME");
-            n = convertColumnName(n);
-            int sqlType = rs.getInt("DATA_TYPE");
-            String sqlTypeName = rs.getString("TYPE_NAME");
-            long precision = rs.getInt("COLUMN_SIZE");
-            precision = convertPrecision(sqlType, precision);
-            int scale = rs.getInt("DECIMAL_DIGITS");
-            scale = convertScale(sqlType, scale);
-            int type = DataType.convertSQLTypeToValueType(sqlType, sqlTypeName);
-            Column col = new Column(n, TypeInfo.getTypeInfo(type, precision, scale, null));
-            col.setTable(this, i++);
-            columnList.add(col);
-            columnMap.put(n, col);
+            rs.close();
         }
-        rs.close();
         if (originalTable.indexOf('.') < 0 && !StringUtils.isNullOrEmpty(schema)) {
             qualifiedTableName = schema + "." + originalTable;
         } else {
@@ -170,12 +174,12 @@ public class TableLink extends Table {
         // check if the table is accessible
 
         try (Statement stat = conn.getConnection().createStatement()) {
-            rs = stat.executeQuery("SELECT * FROM " +
+            ResultSet rs = stat.executeQuery("SELECT * FROM " +
                     qualifiedTableName + " T WHERE 1=0");
             if (columnList.isEmpty()) {
                 // alternative solution
                 ResultSetMetaData rsMeta = rs.getMetaData();
-                for (i = 0; i < rsMeta.getColumnCount();) {
+                for (int i = 0; i < rsMeta.getColumnCount();) {
                     String n = rsMeta.getColumnName(i + 1);
                     n = convertColumnName(n);
                     int sqlType = rsMeta.getColumnType(i + 1);
@@ -201,6 +205,13 @@ public class TableLink extends Table {
         linkedIndex = new LinkedIndex(this, id, IndexColumn.wrap(cols),
                 IndexType.createNonUnique(false));
         indexes.add(linkedIndex);
+        if (!isQuery) {
+            readIndexes(meta, columnMap);
+        }
+    }
+
+    private void readIndexes(DatabaseMetaData meta, HashMap<String, Column> columnMap) throws SQLException {
+        ResultSet rs;
         try {
             rs = meta.getPrimaryKeys(null, originalSchema, originalTable);
         } catch (Exception e) {

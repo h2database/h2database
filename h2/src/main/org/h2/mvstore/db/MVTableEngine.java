5207a566c10c405850e0c472908f46cba1735736
Andrei Tokar <andrei.tokar@gmail.com>
2019-09-15 21:45:52 -0400

    Map MVStore exception code to Database codes

--- h2/src/main/org/h2/mvstore/db/MVTableEngine.java
+++ h2/src/main/org/h2/mvstore/db/MVTableEngine.java
@@ -192,7 +192,11 @@ public class MVTableEngine implements TableEngine {
          */
         DbException convertIllegalStateException(IllegalStateException e) {
             int errorCode = DataUtils.getErrorCode(e.getMessage());
-            if (errorCode == DataUtils.ERROR_FILE_CORRUPT) {
+            if (errorCode == DataUtils.ERROR_CLOSED) {
+                throw DbException.get(
+                        ErrorCode.DATABASE_IS_CLOSED,
+                        e, fileName);
+            } else if (errorCode == DataUtils.ERROR_FILE_CORRUPT) {
                 if (encrypted) {
                     throw DbException.get(
                             ErrorCode.FILE_ENCRYPTION_ERROR_1,
@@ -206,6 +210,10 @@ public class MVTableEngine implements TableEngine {
                 throw DbException.get(
                         ErrorCode.IO_EXCEPTION_1,
                         e, fileName);
+            } else if (errorCode == DataUtils.ERROR_TRANSACTION_ILLEGAL_STATE) {
+                throw DbException.get(
+                        ErrorCode.GENERAL_ERROR_1,
+                        e, e.getMessage());
             } else if (errorCode == DataUtils.ERROR_INTERNAL) {
                 throw DbException.get(
                         ErrorCode.GENERAL_ERROR_1,

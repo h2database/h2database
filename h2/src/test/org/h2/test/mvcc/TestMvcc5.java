802dca3d52244566a78a3eaedc230fecc03582dc
auntyellow <webmaster@xqbase.com>
2020-01-02 23:41:23 +0800

    use try-with-resource

--- h2/src/test/org/h2/test/mvcc/TestMvcc5.java
+++ h2/src/test/org/h2/test/mvcc/TestMvcc5.java
@@ -45,88 +45,74 @@ public class TestMvcc5 extends TestDb {
 
     /** @see https://stackoverflow.com/questions/59441183 */
     private void testSelectAndUpdateConcurrency() throws SQLException {
-    	String isolation = "REPEATABLE READ";
         deleteDb("mvcc5");
-        Connection setup = getConnection("mvcc5");
-        setup.createStatement().execute(
-        		"SET SESSION CHARACTERISTICS AS TRANSACTION ISOLATION LEVEL " + isolation);
+        int level = Connection.TRANSACTION_REPEATABLE_READ;
+        try (
+                Connection setup = getConnection("mvcc5");
+                Connection c1 = getConnection("mvcc5");
+                Connection c2 = getConnection("mvcc5");
+            ) {
+            try (Statement s = setup.createStatement()) {
+                s.executeUpdate("CREATE TABLE test ("
+                        + "id INT NOT NULL PRIMARY KEY, "
+                        + "val INT NOT NULL)");
+                s.executeUpdate("INSERT INTO test (id, val) VALUES (1, 10)");
+            }
 
-        {
-            Statement s = setup.createStatement();
-            s.executeUpdate("CREATE TABLE test ("
-                    + "id INT NOT NULL PRIMARY KEY, "
-                    + "val INT NOT NULL)");
+            c1.setTransactionIsolation(level);
+            c1.setAutoCommit(false);
+            c2.setTransactionIsolation(level);
+            c2.setAutoCommit(false);
 
-            PreparedStatement ps = setup.prepareStatement(
-                    "INSERT INTO test (id, val) VALUES (?, ?)");
-            ps.setInt(1, 1);
-            ps.setInt(2, 10);
-            ps.executeUpdate();
+            // Session A
+            PreparedStatement ps1 = c1.prepareStatement(
+                    "SELECT val FROM test WHERE id = ?");
+            ps1.setInt(1, 1);
+            ResultSet rs1 = ps1.executeQuery();
+            rs1.next();
+            assertEquals(10, rs1.getInt("val"));
+
+            // Session B
+            PreparedStatement ps2 = c2.prepareStatement(
+                    "SELECT val FROM test WHERE id = ?");
+            ps2.setInt(1, 1);
+            ResultSet rs2 = ps2.executeQuery();
+            rs2.next();
+            assertEquals(10, rs2.getInt("val"));
+
+            // Session A
+            PreparedStatement psu1 = c1.prepareStatement(
+                    "UPDATE test SET val = val + ? WHERE id = ?");
+            psu1.setInt(1, 2);
+            psu1.setInt(2, 1);
+            assertEquals(1, psu1.executeUpdate());
+            rs1 = ps1.executeQuery();
+            rs1.next();
+            assertEquals(12, rs1.getInt("val"));
+            c1.commit();
+
+            // Session B
+            rs2 = ps2.executeQuery();
+            rs2.next();
+            // Here we get the expected result because Session B have an isolated 
+            // copy of row id = 1, i.e. the commit from Session A is NOT visible here
+            assertEquals(10, rs2.getInt("val"));
+
+            // Session B
+            PreparedStatement psu2 = c2.prepareStatement(
+                    "UPDATE test SET val = val + ? WHERE id = ?");
+            psu2.setInt(1, 3);
+            psu2.setInt(2, 1);
+            // According to this video https://www.youtube.com/watch?v=sxabCqWsFHg
+            // about MVCC (at 15'00"), this update should be rejected.
+            assertEquals(1, psu2.executeUpdate());
+
+            // Session B
+            rs2 = ps2.executeQuery();
+            rs2.next();
+            // 10 if update is really rejected
+            assertEquals(15, rs2.getInt("val"));
+            c2.rollback();
         }
-
-        // Session A
-        Connection c1 = getConnection("mvcc5");
-        c1.createStatement().execute(
-                "SET SESSION CHARACTERISTICS AS TRANSACTION ISOLATION LEVEL " + isolation);
-        c1.setAutoCommit(false);
-        PreparedStatement ps1 = c1.prepareStatement(
-                "SELECT val FROM test WHERE id = ?");
-        ps1.setInt(1, 1);
-        ResultSet rs1 = ps1.executeQuery();
-        rs1.next();
-        assertEquals(10, rs1.getInt("val"));
-
-        // Session B
-        Connection c2 = getConnection("mvcc5");
-        c2.createStatement().execute(
-        		"SET SESSION CHARACTERISTICS AS TRANSACTION ISOLATION LEVEL " + isolation);
-        c2.setAutoCommit(false);
-        PreparedStatement ps2 = c2.prepareStatement(
-                "SELECT val FROM test WHERE id = ?");
-        ps2.setInt(1, 1);
-        ResultSet rs2 = ps2.executeQuery();
-        rs2.next();
-        assertEquals(10, rs2.getInt("val"));
-
-        // Session A
-        PreparedStatement psu1 = c1.prepareStatement(
-                "UPDATE test SET val = val + ? WHERE id = ?");
-        psu1.setInt(1, 2);
-        psu1.setInt(2, 1);
-        assertEquals(1, psu1.executeUpdate());
-        rs1 = ps1.executeQuery();
-        rs1.next();
-        assertEquals(12, rs1.getInt("val"));
-        c1.commit();
-        c1.close();
-
-        // Session B
-        rs2 = ps2.executeQuery();
-        rs2.next();
-        // Here we get the expected result because Session B have an isolated 
-        // copy of row id = 1, i.e. the commit from Session A is NOT visible here
-        assertEquals(10, rs2.getInt("val"));
-
-        // Session B
-        PreparedStatement psu2 = c2.prepareStatement(
-                "UPDATE test SET val = val + ? WHERE id = ?");
-        psu2.setInt(1, 3);
-        psu2.setInt(2, 1);
-        // According to this video https://www.youtube.com/watch?v=sxabCqWsFHg
-        // about MVCC (at 15'00"), this update should be rejected.
-        assertEquals(1, psu2.executeUpdate());
-
-        // Session B
-        rs2 = ps2.executeQuery();
-        rs2.next();
-        assertEquals(15, rs2.getInt("val"));
-        c2.rollback();
-        c2.close();
-
-        setup.close();
     }
 }
-
-
-
-

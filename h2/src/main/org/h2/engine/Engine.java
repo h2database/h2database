h2database/h2database

commit c5e3654037092704fb243396c07993a7ce9d7a01

Don't check database files multiple times during pre-initialization

h2/src/main/org/h2/engine/Engine.java
@@ -16,6 +16,7 @@ import org.h2.message.Trace;
 import org.h2.security.auth.AuthenticationException;
 import org.h2.security.auth.AuthenticationInfo;
 import org.h2.security.auth.Authenticator;
+import org.h2.store.fs.FileUtils;
 import org.h2.util.MathUtils;
 import org.h2.util.ParserUtil;
 import org.h2.util.ThreadDeadlockDetector;
@@ -60,15 +61,37 @@ public class Engine implements SessionFactory {
                 database = DATABASES.get(name);
             }
             if (database == null) {
-                String p;
-                if (!ci.isPersistent() || !((p = ci.getProperty("MV_STORE")) == null ? Database.exists(name)
-                        : Database.exists(name, Utils.parseBoolean(p, true, false)))) {
-                    if (ifExists) {
-                        throw DbException.get(ErrorCode.DATABASE_NOT_FOUND_WITH_IF_EXISTS_1, name);
+                if (ci.isPersistent()) {
+                    String p = ci.getProperty("MV_STORE");
+                    String fileName;
+                    if (p == null) {
+                        fileName = name + Constants.SUFFIX_MV_FILE;
+                        if (!FileUtils.exists(fileName)) {
+                            fileName = name + Constants.SUFFIX_PAGE_FILE;
+                            if (FileUtils.exists(fileName)) {
+                                ci.setProperty("MV_STORE", "FALSE");
+                            } else {
+                                throwNotFound(ifExists, forbidCreation, name);
+                                fileName = name + Constants.SUFFIX_OLD_DATABASE_FILE;
+                                if (FileUtils.exists(fileName)) {
+                                    throw DbException.getFileVersionError(fileName);
+                                }
+                                fileName = null;
+                            }
+                        }
+                    } else {
+                        fileName = name + (Utils.parseBoolean(p, true, false) ? Constants.SUFFIX_MV_FILE
+                                : Constants.SUFFIX_PAGE_FILE);
+                        if (!FileUtils.exists(fileName)) {
+                            throwNotFound(ifExists, forbidCreation, name);
+                            fileName = null;
+                        }
                     }
-                    if (forbidCreation) {
-                        throw DbException.get(ErrorCode.REMOTE_DATABASE_NOT_FOUND_1, name);
+                    if (fileName != null && !FileUtils.canWrite(fileName)) {
+                        ci.setProperty("ACCESS_MODE_DATA", "r");
                     }
+                } else {
+                    throwNotFound(ifExists, forbidCreation, name);
                 }
                 database = new Database(ci, cipher);
                 opened = true;
@@ -152,6 +175,15 @@ public class Engine implements SessionFactory {
             jmx = true;
         }
         return session;
+    }
+
+    private static void throwNotFound(boolean ifExists, boolean forbidCreation, String name) {
+        if (ifExists) {
+            throw DbException.get(ErrorCode.DATABASE_NOT_FOUND_WITH_IF_EXISTS_1, name);
+        }
+        if (forbidCreation) {
+            throw DbException.get(ErrorCode.REMOTE_DATABASE_NOT_FOUND_1, name);
+        }
     }
 
     /**

h2/src/main/org/h2/engine/Database.java
@@ -454,33 +454,6 @@ public class Database implements DataHandler, CastDataProvider {
     }
 
     /**
-     * Check if a database with the given name exists.
-     *
-     * @param name the name of the database (including path)
-     * @return true if one exists
-     */
-    static boolean exists(String name) {
-        if (FileUtils.exists(name + Constants.SUFFIX_PAGE_FILE)) {
-            return true;
-        }
-        return FileUtils.exists(name + Constants.SUFFIX_MV_FILE);
-    }
-
-    /**
-     * Check if a database with the given name exists.
-     *
-     * @param name
-     *            the name of the database (including path)
-     * @param mvStore
-     *            {@code true} to check MVStore file only, {@code false} to
-     *            check PageStore file only
-     * @return true if one exists
-     */
-    static boolean exists(String name, boolean mvStore) {
-        return FileUtils.exists(name + (mvStore ? Constants.SUFFIX_MV_FILE : Constants.SUFFIX_PAGE_FILE));
-    }
-
-    /**
      * Get the trace object for the given module id.
      *
      * @param moduleId the module id
@@ -547,24 +520,6 @@ public class Database implements DataHandler, CastDataProvider {
 
     private synchronized void open(int traceLevelFile, int traceLevelSystemOut) {
         if (persistent) {
-            String dataFileName = databaseName + Constants.SUFFIX_OLD_DATABASE_FILE;
-            boolean existsData = FileUtils.exists(dataFileName);
-            String pageFileName = databaseName + Constants.SUFFIX_PAGE_FILE;
-            String mvFileName = databaseName + Constants.SUFFIX_MV_FILE;
-            boolean existsPage = FileUtils.exists(pageFileName);
-            boolean existsMv = FileUtils.exists(mvFileName);
-            if (existsData && (!existsPage && !existsMv)) {
-                throw DbException.getFileVersionError(dataFileName);
-            }
-            if (existsPage && !FileUtils.canWrite(pageFileName)) {
-                readOnly = true;
-            }
-            if (existsMv && !FileUtils.canWrite(mvFileName)) {
-                readOnly = true;
-            }
-            if (existsPage && !existsMv) {
-                dbSettings.setMvStore(false);
-            }
             if (readOnly) {
                 if (traceLevelFile >= TraceSystem.DEBUG) {
                     String traceFile = Utils.getProperty("java.io.tmpdir", ".") +

h2/src/main/org/h2/engine/DbSettings.java
@@ -328,7 +328,7 @@ public class DbSettings extends SettingsBase {
      * (default: true).<br />
      * Use the MVStore storage engine.
      */
-    public boolean mvStore = get("MV_STORE", true);
+    public final boolean mvStore = get("MV_STORE", true);
 
     /**
      * Database setting <code>COMPRESS</code>
@@ -365,17 +365,6 @@ public class DbSettings extends SettingsBase {
         HashMap<String, String> settings = getSettings();
         settings.put("DATABASE_TO_LOWER", Boolean.toString(lower));
         settings.put("DATABASE_TO_UPPER", Boolean.toString(upper));
-    }
-
-    /**
-     * Sets the database engine setting.
-     *
-     * @param mvStore
-     *            true for MVStore engine, false for PageStore engine
-     */
-    void setMvStore(boolean mvStore) {
-        this.mvStore = mvStore;
-        set("MV_STORE", mvStore);
     }
 
     /**

commit 85661d890351a17995b6a6e0fac41b3c9d25c3b6

Fix case of reported value (regression in c5e3654037092704fb243396c07993a7ce9d7a01)

h2/src/main/org/h2/engine/Engine.java
@@ -69,7 +69,7 @@ public class Engine implements SessionFactory {
                         if (!FileUtils.exists(fileName)) {
                             fileName = name + Constants.SUFFIX_PAGE_FILE;
                             if (FileUtils.exists(fileName)) {
-                                ci.setProperty("MV_STORE", "FALSE");
+                                ci.setProperty("MV_STORE", "false");
                             } else {
                                 throwNotFound(ifExists, forbidCreation, name);
                                 fileName = name + Constants.SUFFIX_OLD_DATABASE_FILE;
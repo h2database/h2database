ee212e3d432b617a45d9ca769d7d009abe964e04
Anand Tamariya <atamariya@gmail.com>
2020-03-29 19:40:42 +0700

    Fix compilation error

--- h2/src/main/org/h2/util/JdbcUtils.java
+++ h2/src/main/org/h2/util/JdbcUtils.java
@@ -24,6 +24,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Types;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Properties;

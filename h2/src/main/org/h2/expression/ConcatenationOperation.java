60d024acdf9f6d0fecd1046d8baacd19206b36e1
Evgenij Ryazanov <katzyn@gmail.com>
2020-05-07 15:49:07 +0800

    Use CONCAT instead of Oracle-specific logic in ConcatenationOperation

--- h2/src/main/org/h2/expression/ConcatenationOperation.java
+++ h2/src/main/org/h2/expression/ConcatenationOperation.java
@@ -8,6 +8,7 @@ package org.h2.expression;
 import java.util.Arrays;
 
 import org.h2.engine.Session;
+import org.h2.expression.function.Function;
 import org.h2.value.DataType;
 import org.h2.value.TypeInfo;
 import org.h2.value.Value;
@@ -37,46 +38,29 @@ public class ConcatenationOperation extends Operation2 {
     @Override
     public Value getValue(Session session) {
         Value l = left.getValue(session).convertTo(type, session);
-        Value r = right.getValue(session).convertTo(type, session);
-        switch (type.getValueType()) {
-        case Value.ARRAY: {
-            if (l == ValueNull.INSTANCE || r == ValueNull.INSTANCE) {
-                return ValueNull.INSTANCE;
-            }
-            Value[] leftValues = ((ValueArray) l).getList(), rightValues = ((ValueArray) r).getList();
-            int leftLength = leftValues.length, rightLength = rightValues.length;
-            Value[] values = Arrays.copyOf(leftValues, leftLength + rightLength);
-            System.arraycopy(rightValues, 0, values, leftLength, rightLength);
-            return ValueArray.get(values, session);
+        if (l == ValueNull.INSTANCE) {
+            return ValueNull.INSTANCE;
         }
-        case Value.BINARY:
-        case Value.VARBINARY: {
-            if (l == ValueNull.INSTANCE || r == ValueNull.INSTANCE) {
-                return ValueNull.INSTANCE;
-            }
+        Value r = right.getValue(session).convertTo(type, session);
+        if (r == ValueNull.INSTANCE) {
+            return ValueNull.INSTANCE;
+        }
+        int valueType = type.getValueType();
+        if (valueType == Value.VARCHAR) {
+            String s1 = l.getString(), s2 = r.getString();
+            return ValueVarchar.get(new StringBuilder(s1.length() + s2.length()).append(s1).append(s2).toString());
+        } else if (valueType == Value.VARBINARY) {
             byte[] leftBytes = l.getBytesNoCopy(), rightBytes = r.getBytesNoCopy();
             int leftLength = leftBytes.length, rightLength = rightBytes.length;
             byte[] bytes = Arrays.copyOf(leftBytes, leftLength + rightLength);
             System.arraycopy(rightBytes, 0, bytes, leftLength, rightLength);
             return ValueVarbinary.getNoCopy(bytes);
-        }
-        default: {
-            if (l == ValueNull.INSTANCE) {
-                if (session.getMode().treatEmptyStringsAsNull) {
-                    return r;
-                }
-                return ValueNull.INSTANCE;
-            } else if (r == ValueNull.INSTANCE) {
-                if (session.getMode().treatEmptyStringsAsNull) {
-                    return l;
-                }
-                return ValueNull.INSTANCE;
-            }
-            String s1 = l.getString(), s2 = r.getString();
-            StringBuilder buff = new StringBuilder(s1.length() + s2.length());
-            buff.append(s1).append(s2);
-            return ValueVarchar.get(buff.toString());
-        }
+        } else {
+            Value[] leftValues = ((ValueArray) l).getList(), rightValues = ((ValueArray) r).getList();
+            int leftLength = leftValues.length, rightLength = rightValues.length;
+            Value[] values = Arrays.copyOf(leftValues, leftLength + rightLength);
+            System.arraycopy(rightValues, 0, values, leftLength, rightLength);
+            return ValueArray.get(values, session);
         }
     }
 
@@ -87,16 +71,21 @@ public class ConcatenationOperation extends Operation2 {
         TypeInfo l = left.getType(), r = right.getType();
         int lValueType = l.getValueType(), rValueType = r.getValueType();
         if (lValueType == Value.ARRAY || rValueType == Value.ARRAY) {
-            type = TypeInfo.getHigherType(l, r);
-            type = TypeInfo.getTypeInfo(Value.ARRAY, -1, 0, type.getExtTypeInfo());
+            type = TypeInfo.getTypeInfo(Value.ARRAY, -1, 0, TypeInfo.getHigherType(l, r).getExtTypeInfo());
         } else if (DataType.isBinaryStringType(lValueType) && DataType.isBinaryStringType(rValueType)) {
             type = TypeInfo.getTypeInfo(Value.VARBINARY, DataType.addPrecision(l.getPrecision(), r.getPrecision()), 0,
                     null);
-        } else if (DataType.isCharacterStringType(lValueType) && DataType.isCharacterStringType(rValueType)) {
-            type = TypeInfo.getTypeInfo(Value.VARCHAR, DataType.addPrecision(l.getPrecision(), r.getPrecision()), 0,
-                    null);
         } else {
-            type = TypeInfo.TYPE_VARCHAR;
+            if (session.getMode().treatEmptyStringsAsNull) {
+                return Function.getFunctionWithArgs(Function.CONCAT, new Expression[] { left, right })
+                        .optimize(session);
+            }
+            if (DataType.isCharacterStringType(lValueType) && DataType.isCharacterStringType(rValueType)) {
+                type = TypeInfo.getTypeInfo(Value.VARCHAR, DataType.addPrecision(l.getPrecision(), r.getPrecision()),
+                        0, null);
+            } else {
+                type = TypeInfo.TYPE_VARCHAR;
+            }
         }
         if (left.isConstant() && right.isConstant()) {
             return TypedValueExpression.getTypedIfNull(getValue(session), type);

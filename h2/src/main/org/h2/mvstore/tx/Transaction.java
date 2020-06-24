5059679396cacc2ba12d3d0cee098a30023e29e8
Noel Grandin <noelgrandin@gmail.com>
2020-06-24 11:17:35 +0200

    speed SELECT COUNT(*) when using READ_UNCOMMITTED
    which lets me do faster queries on a table that has a high write rate

--- h2/src/main/org/h2/mvstore/tx/Transaction.java
+++ h2/src/main/org/h2/mvstore/tx/Transaction.java
@@ -312,6 +312,10 @@ public final class Transaction {
         return isolationLevel == IsolationLevel.READ_COMMITTED;
     }
 
+    boolean isReadUncommitted() {
+        return isolationLevel == IsolationLevel.READ_UNCOMMITTED;
+    }
+    
     /**
      * Whether this transaction has isolation level READ_COMMITTED or below.
      * @return true if isolation level is READ_COMMITTED or READ_UNCOMMITTED

ea41f7471a67e14b0f855e0de068c2a72c4c1eb0
Andrei Tokar <andrei.tokar@gmail.com>
2020-08-23 23:03:57 -0400

    wrong field size check

--- h2/src/main/org/h2/mvstore/Page.java
+++ h2/src/main/org/h2/mvstore/Page.java
@@ -733,7 +733,7 @@ public abstract class Page<K,V> implements Cloneable {
                     buff.position(compressStart).get(exp);
                 }
                 int compLen = compressor.compress(exp, pos, expLen, comp, 0);
-                int plus = DataUtils.getVarIntLen(compLen - expLen);
+                int plus = DataUtils.getVarIntLen(expLen - compLen);
                 if (compLen + plus < expLen) {
                     buff.position(typePos)
                         .put((byte) (type | compressType));

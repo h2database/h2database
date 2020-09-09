23b51dd84a263cae02221b84ea5b892ce3ba8a84
Evgenij Ryazanov <katzyn@gmail.com>
2020-09-09 12:43:31 +0800

    Fix NPE in InformationSchemaTable.elementTypesFields()

--- h2/src/main/org/h2/table/InformationSchemaTable.java
+++ h2/src/main/org/h2/table/InformationSchemaTable.java
@@ -1500,7 +1500,7 @@ public final class InformationSchemaTable extends MetaTable {
                     FunctionAlias.JavaMethod method = methods[i];
                     TypeInfo typeInfo = method.getDataType();
                     String specificName = name + '_' + (i + 1);
-                    if (typeInfo.getValueType() != Value.NULL) {
+                    if (typeInfo != null && typeInfo.getValueType() != Value.NULL) {
                         elementTypesFieldsRow(session, rows, catalog, type, mainSchemaName, collation,
                                 schemaName, specificName, "ROUTINE", "RESULT", typeInfo);
                     }

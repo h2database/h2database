h2database/h2database

commit beaa4613957b446559fdfbd8cb14301026c7d609

Create JdbcConnection directly in TcpServer

h2/src/main/org/h2/util/JdbcUtils.java
@@ -27,7 +27,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashSet;
-import java.util.Properties;
 import javax.naming.Context;
 import javax.sql.DataSource;
 import org.h2.api.ErrorCode;
@@ -263,14 +262,7 @@ public class JdbcUtils {
      */
     public static Connection getConnection(String driver, String url,
             String user, String password) throws SQLException {
-        Properties prop = new Properties();
-        if (user != null) {
-            prop.setProperty("user", user);
-        }
-        if (password != null) {
-            prop.setProperty("password", password);
-        }
-        return getConnection(driver, url, prop, null);
+        return getConnection(driver, url, user, password, null);
     }
 
     /**
@@ -278,14 +270,15 @@ public class JdbcUtils {
      *
      * @param driver the driver class name
      * @param url the database URL
-     * @param prop the properties containing at least the user name and password
+     * @param user the user name or {@code null}
+     * @param password the password or {@code null}
      * @param networkConnectionInfo the network connection information, or {@code null}
      * @return the database connection
      */
-    public static Connection getConnection(String driver, String url, Properties prop,
+    public static Connection getConnection(String driver, String url, String user, String password,
             NetworkConnectionInfo networkConnectionInfo) throws SQLException {
         if (url.startsWith(Constants.START_URL)) {
-            JdbcConnection connection = new JdbcConnection(url, prop);
+            JdbcConnection connection = new JdbcConnection(url, null, user, password);
             if (networkConnectionInfo != null) {
                 connection.getSession().setNetworkConnectionInfo(networkConnectionInfo);
             }
@@ -303,7 +296,7 @@ public class JdbcUtils {
                      * subprotocol in classpath of jdbc drivers (as example
                      * redshift and postgresql drivers)
                      */
-                    Connection connection = driverInstance.connect(url, prop);
+                    Connection connection = driverInstance.connect(url, null);
                     if (connection != null) {
                         return connection;
                     }
@@ -312,8 +305,6 @@ public class JdbcUtils {
                     // JNDI context
                     Context context = (Context) d.getDeclaredConstructor().newInstance();
                     DataSource ds = (DataSource) context.lookup(url);
-                    String user = prop.getProperty("user");
-                    String password = prop.getProperty("password");
                     if (StringUtils.isNullOrEmpty(user) && StringUtils.isNullOrEmpty(password)) {
                         return ds.getConnection();
                     }
@@ -324,7 +315,7 @@ public class JdbcUtils {
             }
             // don't know, but maybe it loaded a JDBC Driver
         }
-        return DriverManager.getConnection(url, prop);
+        return DriverManager.getConnection(url, user, password);
     }
 
     /**

h2/src/main/org/h2/Driver.java
@@ -56,7 +56,7 @@ public class Driver implements java.sql.Driver, JdbcDriverBackwardsCompat {
         if (url == null) {
             throw DbException.getJdbcSQLException(ErrorCode.URL_FORMAT_ERROR_2, null, Constants.URL_FORMAT, null);
         } else if (url.startsWith(Constants.START_URL)) {
-            return new JdbcConnection(url, info);
+            return new JdbcConnection(url, info, null, null);
         } else if (url.equals(DEFAULT_URL)) {
             return DEFAULT_CONNECTION.get();
         } else {

h2/src/main/org/h2/engine/ConnectionInfo.java
@@ -73,9 +73,13 @@ public class ConnectionInfo implements Cloneable {
      * Create a connection info object.
      *
      * @param u the database URL (must start with jdbc:h2:)
-     * @param info the connection properties
+     * @param info the connection properties or {@code null}
+     * @param user the user name or {@code null}
+     * @param password
+     *            the password as {@code String} or {@code char[]}, or
+     *            {@code null}
      */
-    public ConnectionInfo(String u, Properties info) {
+    public ConnectionInfo(String u, Properties info, String user, Object password) {
         u = remapURL(u);
         this.originalURL = u;
         if (!u.startsWith(Constants.START_URL)) {
@@ -84,6 +88,12 @@ public class ConnectionInfo implements Cloneable {
         this.url = u;
         if (info != null) {
             readProperties(info);
+        }
+        if (user != null) {
+            prop.put("USER", user);
+        }
+        if (password != null) {
+            prop.put("PASSWORD", password);
         }
         readSettingsFromURL();
         Object timeZoneName = prop.remove("TIME ZONE");

h2/src/main/org/h2/jdbc/JdbcConnection.java
@@ -103,9 +103,9 @@ public class JdbcConnection extends TraceObject implements Connection, JdbcConne
      * CloseWatcher.
      */
     @SuppressWarnings("resource")
-    public JdbcConnection(String url, Properties info) throws SQLException {
+    public JdbcConnection(String url, Properties info, String user, Object password) throws SQLException {
         try {
-            ConnectionInfo ci = new ConnectionInfo(url, info);
+            ConnectionInfo ci = new ConnectionInfo(url, info, user, password);
             String baseDir = SysProperties.getBaseDir();
             if (baseDir != null) {
                 ci.setBaseDir(baseDir);
@@ -117,7 +117,7 @@ public class JdbcConnection extends TraceObject implements Connection, JdbcConne
             if (isInfoEnabled()) {
                 trace.infoCode("Connection " + getTraceObjectName()
                         + " = DriverManager.getConnection("
-                        + quote(ci.getOriginalURL()) + ", " + quote(user)
+                        + quote(ci.getOriginalURL()) + ", " + quote(this.user)
                         + ", \"\");");
             }
             this.url = ci.getURL();

h2/src/main/org/h2/server/TcpServer.java
@@ -9,23 +9,19 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.h2.Driver;
 import org.h2.api.ErrorCode;
 import org.h2.engine.Constants;
+import org.h2.jdbc.JdbcConnection;
 import org.h2.message.DbException;
-import org.h2.util.JdbcUtils;
 import org.h2.util.MathUtils;
 import org.h2.util.NetUtils;
 import org.h2.util.NetUtils2;
@@ -66,7 +62,7 @@ public class TcpServer implements Service {
     private boolean allowOthers;
     private boolean isDaemon;
     private boolean ifExists = true;
-    private Connection managementDb;
+    private JdbcConnection managementDb;
     private PreparedStatement managementDbAdd;
     private PreparedStatement managementDbRemove;
     private String managementPassword = "";
@@ -89,12 +85,8 @@ public class TcpServer implements Service {
         if (managementPassword.isEmpty()) {
             managementPassword = StringUtils.convertBytesToHex(MathUtils.secureRandomBytes(32));
         }
-        Properties prop = new Properties();
-        prop.setProperty("user", "");
-        prop.setProperty("password", managementPassword);
         // avoid using the driver manager
-        Connection conn = Driver.load().connect("jdbc:h2:" +
-                getManagementDbName(port), prop);
+        JdbcConnection conn = new JdbcConnection("jdbc:h2:" + getManagementDbName(port), null, "", managementPassword);
         managementDb = conn;
 
         try (Statement stat = conn.createStatement()) {
@@ -459,11 +451,8 @@ public class TcpServer implements Service {
                 throw DbException.convert(e);
             }
             for (int i = 0; i < 2; i++) {
-                Connection conn = null;
-                PreparedStatement prep = null;
-                try {
-                    conn = DriverManager.getConnection("jdbc:h2:" + url + "/" + db, "", password);
-                    prep = conn.prepareStatement("CALL STOP_SERVER(?, ?, ?)");
+                try (JdbcConnection conn = new JdbcConnection("jdbc:h2:" + url + '/' + db, null, "", password)) {
+                    PreparedStatement prep = conn.prepareStatement("CALL STOP_SERVER(?, ?, ?)");
                     prep.setInt(1, all ? 0 : port);
                     prep.setString(2, password);
                     prep.setInt(3, force ? SHUTDOWN_FORCE : SHUTDOWN_NORMAL);
@@ -483,9 +472,6 @@ public class TcpServer implements Service {
                     if (i == 1) {
                         throw e;
                     }
-                } finally {
-                    JdbcUtils.closeSilently(prep);
-                    JdbcUtils.closeSilently(conn);
                 }
             }
         } catch (Exception e) {

h2/src/main/org/h2/server/pg/PgServerThread.java
@@ -267,10 +267,8 @@ public final class PgServerThread implements Runnable {
                 info.put("MODE", "PostgreSQL");
                 info.put("DATABASE_TO_LOWER", "TRUE");
                 info.put("DEFAULT_NULL_ORDERING", "HIGH");
-                info.put("USER", userName);
-                info.put("PASSWORD", password);
                 String url = "jdbc:h2:" + databaseName;
-                ConnectionInfo ci = new ConnectionInfo(url, info);
+                ConnectionInfo ci = new ConnectionInfo(url, info, userName, password);
                 String baseDir = server.getBaseDir();
                 if (baseDir == null) {
                     baseDir = SysProperties.getBaseDir();

h2/src/main/org/h2/server/web/WebServer.java
@@ -773,11 +773,6 @@ public class WebServer implements Service {
             String password, String userKey, NetworkConnectionInfo networkConnectionInfo) throws SQLException {
         driver = driver.trim();
         databaseUrl = databaseUrl.trim();
-        Properties p = new Properties();
-        p.setProperty("user", user.trim());
-        // do not trim the password, otherwise an
-        // encrypted H2 database with empty user password doesn't work
-        p.setProperty("password", password);
         if (databaseUrl.startsWith("jdbc:h2:")) {
             if (!allowSecureCreation || key == null || !key.equals(userKey)) {
                 if (ifExists) {
@@ -785,7 +780,9 @@ public class WebServer implements Service {
                 }
             }
         }
-        return JdbcUtils.getConnection(driver, databaseUrl, p, networkConnectionInfo);
+        // do not trim the password, otherwise an
+        // encrypted H2 database with empty user password doesn't work
+        return JdbcUtils.getConnection(driver, databaseUrl, user.trim(), password, networkConnectionInfo);
     }
 
     /**

h2/src/main/org/h2/store/RecoverTester.java
@@ -10,7 +10,6 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.HashSet;
-import java.util.Properties;
 
 import org.h2.api.ErrorCode;
 import org.h2.engine.ConnectionInfo;
@@ -103,11 +102,8 @@ public class RecoverTester implements Recorder {
             }
             verifyCount++;
             // avoid using the Engine class to avoid deadlocks
-            Properties p = new Properties();
-            p.setProperty("user", "");
-            p.setProperty("password", "");
             ConnectionInfo ci = new ConnectionInfo("jdbc:h2:" + testDatabase +
-                    ";FILE_LOCK=NO;TRACE_LEVEL_FILE=0", p);
+                    ";FILE_LOCK=NO;TRACE_LEVEL_FILE=0", null, "", "");
             Database database = new Database(ci, null);
             // close the database
             SessionLocal sysSession = database.getSystemSession();
@@ -148,9 +144,8 @@ public class RecoverTester implements Recorder {
         try {
             IOUtils.copyFiles(fileName, testDatabase + Constants.SUFFIX_PAGE_FILE);
             // avoid using the Engine class to avoid deadlocks
-            Properties p = new Properties();
             ConnectionInfo ci = new ConnectionInfo("jdbc:h2:" +
-                        testDatabase + ";FILE_LOCK=NO", p);
+                        testDatabase + ";FILE_LOCK=NO", null, null, null);
             Database database = new Database(ci, null);
             // close the database
             database.removeSession(null);

commit 0989316644d789c8a67ece9577c26d0e2c92ed77

Fix regression caused by beaa4613957b446559fdfbd8cb14301026c7d609

h2/src/main/org/h2/util/JdbcUtils.java
@@ -27,6 +27,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashSet;
+import java.util.Properties;
+
 import javax.naming.Context;
 import javax.sql.DataSource;
 import org.h2.api.ErrorCode;
@@ -291,12 +293,19 @@ public class JdbcUtils {
             try {
                 if (java.sql.Driver.class.isAssignableFrom(d)) {
                     Driver driverInstance = (Driver) d.getDeclaredConstructor().newInstance();
+                    Properties prop = new Properties();
+                    if (user != null) {
+                        prop.setProperty("user", user);
+                    }
+                    if (password != null) {
+                        prop.setProperty("password", password);
+                    }
                     /*
                      * fix issue #695 with drivers with the same jdbc
                      * subprotocol in classpath of jdbc drivers (as example
                      * redshift and postgresql drivers)
                      */
-                    Connection connection = driverInstance.connect(url, null);
+                    Connection connection = driverInstance.connect(url, prop);
                     if (connection != null) {
                         return connection;
                     }
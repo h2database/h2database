b5824061be18e84ed0d405a24807ab54783a836a
Andrei Tokar <andrei.tokar@gmail.com>
2021-04-28 22:43:07 -0400

    fix #3036 blob space reclamation on rollback of in-doubt transaction

--- h2/src/main/org/h2/mvstore/tx/TransactionStore.java
+++ h2/src/main/org/h2/mvstore/tx/TransactionStore.java
@@ -175,12 +175,16 @@ public class TransactionStore {
         return store.openMap(TYPE_REGISTRY_NAME, typeRegistryBuilder);
     }
 
+    public void init() {
+        init(ROLLBACK_LISTENER_NONE);
+    }
+
     /**
      * Initialize the store. This is needed before a transaction can be opened.
      * If the transaction store is corrupt, this method can throw an exception,
      * in which case the store can only be used for reading.
      */
-    public void init() {
+    public void init(RollbackListener listener) {
         if (!init) {
             for (String mapName : store.getMapNames()) {
                 if (mapName.startsWith(UNDO_LOG_NAME_PREFIX)) {
@@ -225,7 +229,7 @@ public class TransactionStore {
                                     logId = lastUndoKey == null ? 0 : getLogId(lastUndoKey) + 1;
                                 }
                                 registerTransaction(transactionId, status, name, logId, timeoutMillis, 0,
-                                        IsolationLevel.READ_COMMITTED, ROLLBACK_LISTENER_NONE);
+                                        IsolationLevel.READ_COMMITTED, listener);
                                 continue;
                             }
                         }

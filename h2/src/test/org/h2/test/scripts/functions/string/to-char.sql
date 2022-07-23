3171ec41326a06e0cefbf51bfaae002e67edbc41
Evgenij Ryazanov <katzyn@gmail.com>
2022-07-23 10:26:04 +0800

    Add support of TINYINT and DECFLOAT to TO_CHAR

--- h2/src/test/org/h2/test/scripts/functions/string/to-char.sql
+++ h2/src/test/org/h2/test/scripts/functions/string/to-char.sql
@@ -2,3 +2,9 @@
 -- and the EPL 1.0 (https://h2database.com/html/license.html).
 -- Initial Developer: H2 Group
 --
+
+VALUES '*' || TO_CHAR(CAST(-1 AS TINYINT), '999.99');
+>> * -1.00
+
+VALUES '*' || TO_CHAR(-11E-1, '999.99');
+>> * -1.10

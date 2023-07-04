f3c8222f3d77a8e71db2e6a72ea183c1931bfa6d
Andrei Tokar <andrei.tokar@gmail.com>
2023-07-04 09:36:33 -0400

    version and release date

--- h2/src/main/org/h2/engine/Constants.java
+++ h2/src/main/org/h2/engine/Constants.java
@@ -15,18 +15,18 @@ public class Constants {
     /**
      * The build date is updated for each public release.
      */
-    public static final String BUILD_DATE = "2022-06-13";
+    public static final String BUILD_DATE = "2023-07-04";
 
     /**
      * Sequential version number. Even numbers are used for official releases,
      * odd numbers are used for development builds.
      */
-    public static final int BUILD_ID = 219;
+    public static final int BUILD_ID = 220;
 
     /**
      * Whether this is a snapshot version.
      */
-    public static final boolean BUILD_SNAPSHOT = true;
+    public static final boolean BUILD_SNAPSHOT = false;
 
     /**
      * If H2 is compiled to be included in a product, this should be set to

21bcd49f03cc4a4cb57df3fd383f4b81a87e8f41
yangzj <yangzj@leofs.com.cn>
2023-07-24 12:26:10 +0800

    Fix the NullPointerException caused by accessing 'user' after the session has been closed.

--- h2/src/main/org/h2/table/InformationSchemaTable.java
+++ h2/src/main/org/h2/table/InformationSchemaTable.java
@@ -2847,11 +2847,16 @@ public final class InformationSchemaTable extends MetaTable {
         NetworkConnectionInfo networkConnectionInfo = s.getNetworkConnectionInfo();
         Command command = s.getCurrentCommand();
         int blockingSessionId = s.getBlockingSessionId();
+        //Fix bug the NullPointerException caused by accessing 'user' after the session has been closed.
+        User user = s.getUser();
+        if(user==null) {
+            return;
+        }
         add(session, rows,
                 // SESSION_ID
                 ValueInteger.get(s.getId()),
                 // USER_NAME
-                s.getUser().getName(),
+                user.getName(),
                 // SERVER
                 networkConnectionInfo == null ? null : networkConnectionInfo.getServer(),
                 // CLIENT_ADDR

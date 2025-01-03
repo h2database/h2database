9136b9f23b1eefb1f99bd6dd12c63cc864cf4d82
Evgenij Ryazanov <katzyn@gmail.com>
2025-01-03 11:38:44 +0800

    Update copyright years

--- h2/src/test/org/h2/test/scripts/functions/aggregate/array_agg.sql
+++ h2/src/test/org/h2/test/scripts/functions/aggregate/array_agg.sql
@@ -1,4 +1,4 @@
--- Copyright 2004-2024 H2 Group. Multiple-Licensed under the MPL 2.0,
+-- Copyright 2004-2025 H2 Group. Multiple-Licensed under the MPL 2.0,
 -- and the EPL 1.0 (https://h2database.com/html/license.html).
 -- Initial Developer: Alex Nordlund
 --
--- h2/src/test/org/h2/test/unit/TestDateIso8601.java
+++ h2/src/test/org/h2/test/unit/TestDateIso8601.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2024 H2 Group. Multiple-Licensed under the MPL 2.0,
+ * Copyright 2004-2025 H2 Group. Multiple-Licensed under the MPL 2.0,
  * and the EPL 1.0 (https://h2database.com/html/license.html).
  * Initial Developer: Robert Rathsack (firstName dot lastName at gmx dot de)
  */
